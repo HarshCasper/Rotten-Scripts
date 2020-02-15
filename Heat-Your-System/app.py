3526fc9cc7f6bd667f85602ceb2cc2e76762ec9a
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-02-16 02:36:01 +0530

    Create app.py

---
+++ Heat-Your-System/app.py
@@ -0,0 +1,16 @@
+# Python Script to Heat your CPU Cores in Cold Temperatures
+
+from multiprocessing import Process, cpu_count
+from sys import argv
+
+defaultProcessCount: int = cpu_count()
+processCount: int = int(argv[1]) if len(argv) > 1 else defaultProcessCount
+
+def heat():
+    for i in range(1 * 10**16):
+        i = i ** i
+        
+for i in range(processCount):
+    print("Starting process number:", i + 1)
+    t = Process(target=heat)
+    t.start()
