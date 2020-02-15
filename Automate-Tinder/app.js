41b2e9475c8eba58a04b44be4fa197a063394a85
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-02-16 03:22:41 +0530

    Create app.js

---
+++ Automate-Tinder/app.js
@@ -0,0 +1,12 @@
+/*
+
+Too bored of Right-Swiping Tinder? This Script allows you to automate Right-Swipes on Tinder. You just need to go to the Official Website
+and kickstart the Developer Console and push the Code. Press ENTER and let the magic redeem itself. 
+
+*/ 
+
+a = setInterval(
+    function(){
+          var elem = document.getElementsByClassName("button Lts($ls-s) Z(0) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) Wc($transform) End(15px) Scale(1.1):h");
+          elem[0].click()
+          },2000)
