00c510f1798adc2d3df9ff1240ef26dab09f3db6
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-06-07 21:14:08 +0530

    Delete osinfo.ps1

--- Operating System Information/osinfo.ps1
+++
@@ -1,5 +0,0 @@
-#.ps1   
-    $wmiobject =  get-wmiobject -class win32_operatingsystem
-    $user = ($wmiobject).RegisteredUser
-    $kernal_version = ($wmiobject).Version
-    Write-host -fore green "The victim is running on kernal $kernal_version ","With the username $user & Computername is $(whoami) ."
