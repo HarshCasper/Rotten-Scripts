8271b5e1c37e473345be89aec98683a7cef43db8
Seema Saharan <48756444+seema1711@users.noreply.github.com>
2020-06-20 20:34:46 +0530

    Lorem Ipsum Generator #2

---
+++ Lorem Ipsum Generator/README.md
@@ -0,0 +1,13 @@
+# Lorem Ipsum Text Generator  
+Don't have the demo text to your website?  
+No worries, this will help you!  
+This script is used to generate the given number of paragraphs of Lorem Ipsum text.  
+
+### Prerequisites:  
+
+pip install lorem_text  
+
+![image](https://github.com)
+
+
+
---
+++ Lorem Ipsum Generator/generator.py
@@ -0,0 +1,11 @@
+# import the lorem module from lorem_text
+from lorem_text import lorem             
+
+# Provide the number of paragrpahs you want
+paragraph_length = int(input("Enter the length of paragraph you want: "))
+
+# Store it in a variable, lorem.paragraphs() is used to generate the paragraphs
+se = lorem.paragraphs(paragraph_length)
+
+# Finally print the paragraphs
+print(se)
