08e6602569e842b937487e4708babf2a200a39c5
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-06-24 12:26:33 +0530

    Remove invalid character

--- Download-Instagram-Posts/main.py
+++ Download-Instagram-Posts/main.py
@@ -17,7 +17,7 @@ driver = webdriver.Firefox(options=options)
 def mkdir_p(path):
   try:
     os.makedirs(path)
-  except OSError as exc:  # Python ≥ 2.5
+  except OSError as exc:  # Python >= 2.5
     if exc.errno == errno.EEXIST and os.path.isdir(path):
       pass
     else:
