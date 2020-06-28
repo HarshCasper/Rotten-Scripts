1b1f04c55ec6e8f66e2fce356944fb28f01883a3
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-06-28 20:08:54 +0530

    Url as argument

--- Find-all-Links/main.py
+++ Find-all-Links/main.py
@@ -1,14 +1,25 @@
 import re
+import sys
 import requests
 
 from bs4 import BeautifulSoup
 
-url = "https://github.com/HarshCasper/Rotten-Scripts/issues/19"
+def help():
+  print("Usage: python main.py <url>")
+
+if len(sys.argv) < 2:
+  help()
+  exit(0)
+
+url = sys.argv[1]
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 
 links = re.findall("http.?://[^\s\"\']+", str(soup))
+
+if len(links) == 0:
+  print("No links on {}".format(url))
 
 for link in links:
   print(link)
