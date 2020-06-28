3f6ed909b281e7dff1f37e7d3bd7737c46d5504d
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-06-28 20:25:19 +0530

    Add functions parse url

--- Find-all-Links/main.py
+++ Find-all-Links/main.py
@@ -7,19 +7,27 @@ from bs4 import BeautifulSoup
 def help():
   print("Usage: python main.py <url>")
 
-if len(sys.argv) < 2:
-  help()
-  exit(0)
+def parseURL(url):
+  return url.replace("&amp;", "&")
 
-url = sys.argv[1]
+def extractLinks(url):
+  # Extracts links from the given webpage
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+  r = requests.get(url)
+  soup = BeautifulSoup(r.content, 'html.parser')
 
-links = re.findall("http.?://[^\s\"\']+", str(soup))
+  links = re.findall("http.?://[^\s\"\']+", str(soup))
 
-if len(links) == 0:
-  print("No links on {}".format(url))
+  if len(links) == 0:
+    print("No links on {}".format(url))
 
-for link in links:
-  print(link)
+  for link in links:
+    print(parseURL(link))
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    help()
+    exit(0)
+
+  url = sys.argv[1]
+  extractLinks(url)
