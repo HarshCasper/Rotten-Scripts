7814a13e076bcbdf7cd175e411065613d5f92123
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2020-07-08 20:45:45 +0530

    Add file.pdf to output_images_folder_name/page_no.jpg convertor code

--- PDF Generator/pdf_generator.py
+++ PDF Generator/pdf_generator.py
@@ -1,11 +1,14 @@
 import os
 import img2pdf
+from pdf2image import convert_from_path
 
-'''
-- Change "output_filename.pdf" with the name of the output pdf file you want
-- "os.listdir('.')" points to the present working directory, change it to "os.listdir('the/directory/path/where/you/want/to/get/images/from')"
-- Change "i.endswith(".jpg")" to your desired input image format
-'''
-
+# *.jpg to output_filename.pdf convertor
 with open("output_filename.pdf", "wb") as f:
     f.write(img2pdf.convert([i for i in os.listdir('.') if i.endswith(".jpg")]))
+
+# file.pdf to output_images_folder_name/page_no.jpg convertor
+pages = convert_from_path('file.pdf', 500)
+page_no = 1
+for page in pages:
+    pages[i].save('output_images_folder_name/page_{}.jpg'.format(page_no), 'JPEG')      # output_images_folder_name = folder needs to be created manually to store all images
+    page_no += 1
