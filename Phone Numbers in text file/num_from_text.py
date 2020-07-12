47f485ac12034df0c5660b0a09017c4ba24661de
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2020-07-12 12:35:35 +0530

    Format output stored in output file

--- Phone Numbers in text file/num_from_text.py
+++ Phone Numbers in text file/num_from_text.py
@@ -3,18 +3,23 @@ import re
 mobile_num = re.compile(r'\b\d{5}-\d{5}\b')
 phone_num = re.compile(r'\b\d{3}-\d{7}\b')
 
-output_file = open('valid_mobile_number.txt', 'w')
+output_file = open('valid_number.txt', 'w')
 
+output_file.write('Valid Mobile Numbers\n')
 with open("kjv10.txt", "r") as input_file:
     for line in input_file:
         valid_mobile_num = mobile_num.findall(line)
-        valid_phone_num = phone_num.findall(line)
         for mnum in valid_mobile_num:
             print(mnum)
-            output_file.write(mnum)
+            output_file.write(mnum+'\n')
+
+output_file.write('\nValid Phone Numbers\n')
+with open("kjv10.txt", "r") as input_file:
+    for line in input_file:
+        valid_phone_num = phone_num.findall(line)
         for pnum in valid_phone_num:
             print(pnum)
-            output_file.write(pnum)
+            output_file.write(pnum+'\n')
 
 input_file.close()
 output_file.close()
