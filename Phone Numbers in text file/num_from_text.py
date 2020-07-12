bc697b5a6a973eefd52f6b5bd1cfb6b9e72f385d
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2020-07-12 12:42:11 +0530

    Modularize code, create functions

--- Phone Numbers in text file/num_from_text.py
+++ Phone Numbers in text file/num_from_text.py
@@ -13,23 +13,60 @@ args = parser.parse_args()
 mobile_num = re.compile(r'\b\d{5}-\d{5}\b')     # REGEX for mobile numbers
 phone_num = re.compile(r'\b\d{3}-\d{7}\b')      # REGEX for phone numbers
 
-input_file = open('kjv10.txt', 'r')             # Open input file
-output_file = open('valid_number.txt', 'w')     # Open output file
+def find_valid_mobile_number():
+    input_file = open(args.mobile, 'r')             # Open input file
+    output_file = open('valid_mobile_numbers.txt', 'w')     # Open output file
+    output_file.write('Valid Mobile Numbers\n')
+    # Valid mobile numbers
+    for line in input_file:
+        valid_mobile_num = mobile_num.findall(line)
+        for mnum in valid_mobile_num:
+            output_file.write(mnum+'\n')
+    # Close files
+    input_file.close()
+    output_file.close()
 
-output_file.write('Valid Mobile Numbers\n')
-# Valid mobile numbers
-for line in input_file:
-    valid_mobile_num = mobile_num.findall(line)
-    for mnum in valid_mobile_num:
-        output_file.write(mnum+'\n')
+def find_valid_phone_number():
+    input_file = open(args.phone, 'r')             # Open input file
+    output_file = open('valid_phone_numbers.txt', 'w')     # Open output file
+    output_file.write('\nValid Phone Numbers\n')
+    # Valid phone numbers
+    for line in input_file:
+        valid_phone_num = phone_num.findall(line)
+        for pnum in valid_phone_num:
+            output_file.write(pnum+'\n')
+    # Close files
+    input_file.close()
+    output_file.close()
 
-output_file.write('\nValid Phone Numbers\n')
-# Valid phone numbers
-for line in input_file:
-    valid_phone_num = phone_num.findall(line)
-    for pnum in valid_phone_num:
-        output_file.write(pnum+'\n')
+def find_all_valid_number():
+    input_file = open(args.all, 'r')             # Open input file
+    output_file = open('valid_numbers.txt', 'w')     # Open output file
+    output_file.write('Valid Mobile Numbers\n')
+    # Valid mobile numbers
+    for line in input_file:
+        valid_mobile_num = mobile_num.findall(line)
+        for mnum in valid_mobile_num:
+            output_file.write(mnum+'\n')
+    output_file.write('\nValid Phone Numbers\n')
+    # Valid phone numbers
+    for line in input_file:
+        valid_phone_num = phone_num.findall(line)
+        for pnum in valid_phone_num:
+            output_file.write(pnum+'\n')
+    # Close files
+    input_file.close()
+    output_file.close()
 
-# Close files
-input_file.close()
-output_file.close()
+def main():
+    if args.mobile is not None:
+        find_valid_mobile_number()
+    elif args.phone is not None:
+        find_valid_phone_number()
+    elif args.all is not None:
+        find_all_valid_number()
+    else:
+        print('Oh well ; No args, no problems')
+
+if __name__ == '__main__':
+    main()
