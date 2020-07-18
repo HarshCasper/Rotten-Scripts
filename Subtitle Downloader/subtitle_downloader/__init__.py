ffd30e2fbafa526cf6d5a4bc9e349402f8f07ee6
Seema Saharan <48756444+seema1711@users.noreply.github.com>
2020-07-18 18:53:04 +0530

    Resolves #12

---
+++ Subtitle Downloader/subtitle_downloader/__init__.py
@@ -0,0 +1,1 @@
+from .subtitle import main
---
+++ Subtitle Downloader/subtitle_downloader/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import sys
+
+setup(name = 'subtitle-downloader-python3',
+    description = 'Python tool to download subtitles for movies/series',
+    version = '0.1.0',
+    author = 'Seema Saharan',
+    authorEmail = 'seemasaran642@gmail.com',
+    entry_points = {
+        'console_scripts': ['subtitle-downloader-python3=subtitle_downloader_python3:main'],
+    },
+    url = 'https://github.com/seema1711/subtitle-downloader/',
+    keywords = ['subtitle','download','utitlity','movie'],
+    classifiers = [
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    )
