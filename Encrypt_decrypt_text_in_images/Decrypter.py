94200198b48c48f9a47572491135dd8185a212bd
WinterSoldier <ayushsingh1315@gmail.com>
2020-07-28 11:23:39 +0530

    Update Decrypter.py

--- Encrypt_decrypt_text_in_images/Decrypter.py
+++ Encrypt_decrypt_text_in_images/Decrypter.py
@@ -2,24 +2,18 @@ import cv2
 import numpy as np
 import time
 
-
 now = time.time()
-print('It\'s happening........ Please wait!!!')
-
 img2 = cv2.imread('output.png')
 
 str=''
 arr=[]
 arr1 =[]
 c=0
-
 def two_dim_conv(x):
     c=0
     for i in x:
         for j in i:
             for k in j:
-
-
                 if c>img2[0][0][0]:
                     break
                 arr.append(k)
@@ -27,14 +21,11 @@ def two_dim_conv(x):
     return arr
 
 two_dim_conv(img2)
-
 l = arr[0]
 string_arr = arr[1:l+1]
 
 for i in string_arr:
     str += chr(i)
-
-
 
 print(str)
 print("Intercercepted message length >>> ",end=' ')
