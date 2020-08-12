69acdc9b20a460966f0379021cea46dffac42766
srimani-programmer <srimani.scholar@gmail.com>
2020-08-12 22:20:44 +0530

    Implemented the Python Library Downloader (ML and Data Science).

---
+++ Python Library Downloader/script.py
@@ -0,0 +1,15 @@
+import os
+import platform
+
+if(platform.system()[1].lower() == 'windows'):
+    # For Windows Operating System.
+    try:
+        os.system("pip install -r Windows/requirements.txt")
+    except Exception:
+        print("Something went Wrong.!")
+else:
+    # Unix Based Systems i.e include (Linux/Mac OSX) etc
+    try:
+        os.system("pip3 install -r Unix/requirements.txt")
+    except Exception:
+        print("Something went Wrong.!")
