495fa5bd06afa070d70f8d747ea3ba2df2572a43
Neo-Techies <mbcse50@gmail.com>
2020-08-15 16:14:19 +0530

    Code updated

---
+++ Extract_Comments/README.md
@@ -0,0 +1,15 @@
+# Extracting Comments From source code in Nodejs
+
+## About Script
+Comments Plays an important role while writing effecient and clean code. Comments provide a great understanding of what is happening in the code and provides better readability. while working on big codebases a lot of comments are present. To automate fetching of comments from source file for documentation and other purposes this script is made
+
+## To run the code
+- Clone the folder
+- Inside the folder open command line and run
+- npm install
+- node extract.js < Your source code file path>
+
+## Output 
+You will see the Line, Block and all type of comments present in file on the console 
+![image](https://github.com/mbcse/Rotten-Scripts/blob/crypto_price/All_Crypto_Price/cryptoprice.png)
+code by Mohit Bhat(https://www.mbcse.co)
--- Extract_Comments/extract.js
+++ Extract_Comments/extract.js
@@ -1,9 +1,12 @@
 var fs=require('fs');
-var extract = require('extract-comments');
+var extract = require('extract-comments');//Library to Extract Comments from source code
 const colors = require('colors/safe');
 const { argv } = require('process');
 const { Console } = require('console');
+
+
 if(process.argv[2]!==undefined){
+//Reading File    
     fs.readFile(process.argv[2],(err,data)=>{
         if(err){
             console.log(colors.red("Error: ")+"Unable To Process File");
@@ -11,7 +14,7 @@ if(process.argv[2]!==undefined){
         else{
     
             var sc=data.toString();
-            var comments=extract(sc);
+            var comments=extract(sc);//Extracting Comments
             for(var i=0;i<comments.length;i++){
                 console.log(colors.green(comments[i].type)+": "+comments[i].value);    
             }
