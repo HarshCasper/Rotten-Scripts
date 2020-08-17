5abfb83c40eaf4d64457f79f071d7303e70f9de1
ANSHUMALI SHAW <37397899+ankugolu@users.noreply.github.com>
2020-08-17 16:19:54 +0530

    Added complete Functionalites to the app

--- WhatsappSmsBomber/main.py
+++ WhatsappSmsBomber/main.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import mycontacts
 import tkinter as tk
 from tkinter import *
+import xpath
+import time
 
 screen = tk.Tk(screenName="WhatsappBomber App")
 screen.geometry("1600x640")
@@ -9,12 +13,59 @@ screen.title("WhatsApp SMS Bomber")
 screen.configure(bg="#1A1A1D")
 
 
+#Defining Function SendMessage
 def sendmessg():
     driver= webdriver.Chrome()
     d1= driver.get("https://web.whatsapp.com/")
     driver.maximize_window()
+    time.sleep(10)
 
+    #Defining Function checkuser which checks that the contact added ireally exists in the contact list or not
+    def checkuser(str):
+        try:
+            driver.find_element_by_xpath(str)
+        except NoSuchElementException:
+            return False
+        return True
+
+    for contact in listcheck:
+        if(contact.get() != ""):
+            chat=driver.find_element_by_xpath(xpath.newchat_xpath)
+            chat.click()
+
+            search=driver.find_element_by_xpath(xpath.search_xpath)
+            search.send_keys(contact.get())
+            time.sleep(1)
+
+            if(checkuser("//span[@title='{}']".format(contact.get())) == False):
+                l2=Label(screen, text="No user in your contact list named " + contact.get(), height = 2, width= 100, bg="#EA2511" , fg= "#050506")
+                l2.grid(row = 10, column= 0, columnspan=10)
+                l2.after(3000, lambda: l2.destroy())
+                continue
+
+            find_user =driver.find_element_by_xpath("//span[@title='{}']".format(contact.get()))
+            find_user.click()
+            time.sleep(1)
+
+            find_message=driver.find_element_by_xpath(xpath.message_xpath)
+            find_message.click()
+            time.sleep(1)
+
+            for i in range(int(count_entry.get())):
+                find_message.send_keys(message_entry.get())
+                driver.find_element_by_xpath(xpath.sendbutton_xpath).click()
+                time.sleep(0.5)
+        time.sleep(1)
+    l1=Label(screen, text="Sms Bombing Completed", height = 2, width= 100, bg="#2ECA12" , fg= "#DEF031")
+    l1.grid(row = 11, column= 0, columnspan=10)
+    l1.after(3000, lambda: l1.destroy())
+
+    #clearing fields
+    message_entry.delete(0,END)
+    count_entry.delete(0,END)
     return
+
+
 
 #Creating a Banner for the main Screen using Label and positioning it on the screen using grid
 banner = Label(screen, text= "Whatsapp SMS BOMBER", height = 3, width= 100, bg= "#1A1A1D", fg= "#c3073f")
@@ -53,7 +104,7 @@ count_entry = Entry(width= 100)
 message_entry.grid(row = 5, column = 1,pady=10, ipady=6,columnspan=2)
 count_entry.grid(row = 6, column = 1, pady=10, ipady=6, columnspan=2)
 
-
+#creating Send Now Button
 send_now = Button(screen, text="Send Now", command = sendmessg, width= 40, bg="#6F2232", fg= "#000000")
 send_now.grid(row =8, column = 2, ipady=6, pady=10)
 
