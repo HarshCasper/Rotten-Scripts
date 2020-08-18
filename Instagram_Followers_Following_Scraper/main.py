3f3421c89a728aad616708ba374283e20be4fb7c
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-08-18 17:20:26 +0530

    add comments

--- Instagram_Followers_Following_Scraper/main.py
+++ Instagram_Followers_Following_Scraper/main.py
@@ -6,29 +6,38 @@ import pandas as pd
 
 
 def get_users(user_type, insta_id):
+    '''Takes the user_type( a string named followers or following) 
+    and a username as argument and returns an array of the user's 
+    followers or following repectively'''
     button = browser.find_element_by_css_selector(
-        "a[href='/"+insta_id+"/"+user_type+"/']")
-    no = (int)(button.text.strip().split()[0])
-    print(no)
-    button.click()
-    users = browser.find_element_by_class_name("PZuss")
-    users = users.find_elements_by_css_selector("li")
+        "a[href='/"+insta_id+"/"+user_type+"/']")  # select followers/following button
+    no = (int)(button.text.strip().split()[0])  # count of followers/following
+    button.click()  # click on followers/following button to open dialog
+    users = browser.find_element_by_class_name(
+        "PZuss")  # for selecting the dialog
+    users = users.find_elements_by_css_selector(
+        "li")  # for getting list of users
     view = browser.find_element_by_class_name("isgrP")
     actionChain = webdriver.ActionChains(browser)
+    # for handeling scroll event on dialog
     actionChain.context_click(on_element=view)
-    n = len(users)
+    n = len(users)  # no of followers/following in the list
+    # while number of followers in the list is less than total number of followers of the user
     while(n < no):
-        actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+        actionChain.key_down(Keys.SPACE).key_up(
+            Keys.SPACE).perform()  # for scrolling down
         users = browser.find_element_by_class_name("PZuss")
-        users = users.find_elements_by_css_selector("li")
-        n = len(users)
+        users = users.find_elements_by_css_selector("li")  # selecting users
+        n = len(users)  # updating the number of users in the list
     for j in range(len(users)):
-        users[j] = users[j].text
+        users[j] = users[j].text  # extracting text
     browser.get("https://www.instagram.com/"+insta_id)
-    return users
+    return users  # return array of all the followers/following of the user
 
 
 def convert_to_csv(followers, following, insta_id):
+    '''Takes arrays of followers and following and a username 
+    as arguments and creates <username>.csv file to store the data'''
     final_arr = []
     for i in range(0, max(len(followers), len(following))):
         follower_account = ""
@@ -48,8 +57,8 @@ def convert_to_csv(followers, following, insta_id):
             "Following Name": following_name,
         }
         final_arr.append(user)
-    df = pd.DataFrame(final_arr)
-    df.to_csv(insta_id+".csv", index=None)
+    df = pd.DataFrame(final_arr)  # convert to data frame
+    df.to_csv(insta_id+".csv", index=None)  # convert to csv
 
 
 if __name__ == "__main__":
@@ -57,7 +66,7 @@ if __name__ == "__main__":
     password = input("Enter your Instagram password: ")
     insta_id = input("Enter user's Instagram username for scraping: ")
 
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    PATH = "C:\Program Files (x86)\chromedriver.exe"  # path to chromedriver
     browser = webdriver.Chrome(PATH)
     browser.implicitly_wait(5)
     browser.get("https://www.instagram.com/")
@@ -67,17 +76,21 @@ if __name__ == "__main__":
     password_input = browser.find_element_by_css_selector(
         "input[name='password']")
 
+    # enters username and password
     username_input.send_keys(username)
     password_input.send_keys(password)
 
+    # click on submit button
     login_button = browser.find_element_by_xpath("//button[@type='submit']")
     login_button.click()
 
+    # save password dialog
     not_now_button = browser.find_element_by_xpath(
         "//button[text()='Not Now']")
     not_now_button.click()
     sleep(2)
 
+    # get user's instagram page
     browser.get("https://www.instagram.com/"+insta_id)
     sleep(2)
 
