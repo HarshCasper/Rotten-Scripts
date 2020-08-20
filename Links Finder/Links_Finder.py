365f50addfcbee8e7079f6f126cc5ac9ef3c5231
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-08-20 21:34:36 +0530

    Create Links_Finder.py

---
+++ Links Finder/Links_Finder.py
@@ -0,0 +1,18 @@
+import requests
+import re
+
+# get url
+url = input('Enter a URL (include `http://`): ')
+
+# connect to the url
+website = requests.get(url)
+
+# read html
+html = website.text
+
+# use re.findall to grab all the links
+links = re.findall('"((http|ftp)s?://.*?)"', html)
+
+# output links
+for link in links:
+    print(link[0])
