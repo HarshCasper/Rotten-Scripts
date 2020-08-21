36df413fab6fdd0b59f511a4fe25433dc7c99dde
Prahitha <prahitha.movva03@gmail.com>
2020-08-21 22:45:39 +0530

    added script to get associated time depending upon timezone

---
+++ Time Conversion/script.py
@@ -0,0 +1,27 @@
+from pytz import timezone, country_timezones
+from datetime import datetime, timedelta
+import argparse
+import pytz
+
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('--location', action='store', type=str, required=False, help="Enter the location")
+my_parser.add_argument('--country', action='store', type=str, required=False, help="Enter the country code")
+
+args = my_parser.parse_args()
+location = args.location
+country = args.country
+
+if (not location and (not country)):
+    print("Please enter either location or the country code")
+    exit()
+
+if (country):
+    code = country_timezones(country)
+    timezone = pytz.timezone(code[0])
+    datetimeFinder = datetime.now(timezone)
+    print("Time in " + str(timezone) + ": " + datetimeFinder.strftime("%H:%M:%S"))
+
+if (location):
+    timezone = pytz.timezone(location)
+    datetimeFinder = datetime.now(timezone)
+    print("Time in " + location + ": " + datetimeFinder.strftime("%H:%M:%S"))
