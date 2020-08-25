89b3294aacc25da711a025f61e7e7b7041b645c1
Anushka-S10 <61749789+Anushka-S10@users.noreply.github.com>
2020-08-25 23:02:35 +0530

    Update web_crawler.py

--- Web Crawler in Python/web_crawler.py
+++ Web Crawler in Python/web_crawler.py
@@ -2,94 +2,77 @@
 # import the packages
 
 import requests
+import re
 from bs4 import BeautifulSoup
-from queue import Queue, Empty
-from concurrent.futures import ThreadPoolExecutor
-from urllib.parse import urljoin, urlparse
+
+# link for scraping data
+# perform a get request for the link
+source = requests.get('https://www.ebay.com/b/Cell-Phones-Smartphones/9355/bn_320094?rt=nc&_pgn=1').text    
+
+def crawler():
+
+    # parsing it with Beautiful Soup
+    soup = BeautifulSoup(source, 'lxml')
+    # declaring items variable to make it more concise for a particular portion 
+    items = soup.find('li', class_='s-item')
 
 
-class MultiThreadScraper:
+    # six categories for the particular product are mention for scrapping purpose
+    # finds all the item from the list and store it 
+    for items in soup.find_all('li', class_='s-item'):
 
-    def __init__(self, base_url):
+        # title of the product
+        try:  
+            item_title = items.find('h3', class_='s-item__title').text
+        except Exception as e:
+            item_title = 'None'
+        print(item_title)
 
-        self.base_url = base_url
-        # root_url ensure that scraper does not end up on other sites
-        self.root_url = '{}://{}'.format(urlparse(self.base_url).scheme, urlparse(self.base_url).netloc)         # urlparse to pull out the site homepage
-        # threadpool allows to use a callback function to collect results
-        self.pool = ThreadPoolExecutor(max_workers=20)    
-        # set contains a list of URLs which has been crawled
-        # prevent the crawler from visiting the same URL twice
-        self.scraped_pages = set([])
-        # queue will contain URLs we wish to crawl, we will continue to grab URLs from our queue until it’s empty
-        self.to_crawl = Queue()
-        # place in the base URL to the start of the queue
-        self.to_crawl.put(self.base_url)
-
-
-# to extract all of a sites internal links
-
-    def parse_links(self, html):
-        # generate soup object using Beautifulsoup
-        soup = BeautifulSoup(html, 'html.parser')
-        # find_all method returns every ‘a’ element which has a ‘href’ property
-        links = soup.find_all('a', href=True)
-
-        for link in links:
-            url = link['href']      # pull out the acual href content
-        
-            #  check whether link is relative (starting with a ‘/’) or starts with our root URL
-            if url.startswith('/') or url.startswith(self.root_url):
-                # urljoin generates a crawlable URL and then put this in the queue provided that hasn't been crawled
-                url = urljoin(self.root_url, url)
-                if url not in self.scraped_pages:
-                    self.to_crawl.put(url)
-
-    # scrape_info method can be overridden so that it can extract data from the site you are crawling
-    def scrape_info(self, html):
-        return
-
-    def post_scrape_callback(self, res):
-
-        # by calling .result() in argument, we can get to the contents of the returned value
-        result = res.result()
-
-        # check if we have a result and whether the result has a 200 status code.
-        # if both of these will be true, send the html to the parse_links and currently empty scrape_info function
-        if result and result.status_code == 200:
-            self.parse_links(result.text)
-            self.scrape_info(result.text)
-
-    # it takes URL and returns a response object if it was successful
-    def scrape_page(self, url):
+        # price of the product
         try:
-            # By limiting the amount of CPU work, we can increase the overall speed of the crawler
-            res = requests.get(url, timeout=(3, 30))
-            return res
-        except requests.RequestException:
-            return
+            item_price = items.find('span', class_='s-item__price').text
+        except Exception as e:
+            item_price = 'None'
+        print(item_price)
 
-# run scraper function brings all the previous work together and manages the thread pool
-    def run_scraper(self):
-        # run scraper will continue to run while there are still URLs to crawl
-        # create while True loop and ignoring any exceptions except Empty, which will be thrown if our queue has been empty for more than 60 seconds
-        while True:
-            try:
-                target_url = self.to_crawl.get(timeout=60)
+        # shipping price information
+        try:
+            item_shipping = items.find('span', class_='s-item__shipping s-item__logisticsCost').text
+        except Exception as e:
+            item_shipping = 'None'
+        print(item_shipping)
+           
+        # rating of the product
+        try:
+            item_stars = items.find('span', class_='clipped').text.split(' ')[0]
+        except Exception as e:
+            item_stars = 'None'
+        print(item_stars)
+        
+        # checks whether quantity has been sold or not
+        try:
+            item_qty_sold = items.find('span', class_='s-item__hotness s-item__itemHotness').text.split(' ')
+            
+            if item_qty_sold[1] == 'sold':
+                item_qty_sold = item_qty_sold[0]
+            else:
+                item_qty_sold = 0
+        except Exception as e:
+            item_qty_sold = 'None' 
+        print(item_qty_sold)
 
-                #  pull URLs from queue and submit them to the thread pool for execution
-                if target_url not in self.scraped_pages:
-                    print("Scraping URL: {}".format(target_url))
-                    self.scraped_pages.add(target_url)
-                    job = self.pool.submit(self.scrape_page, target_url)
-                    # add callback which will run once the function has returned
-                    job.add_done_callback(self.post_scrape_callback)
-            except Empty:
-                return
-            except Exception as e:
-                print(e)
-                continue
+        # retrieving the link of the product
+        try:
+            item_link = items.find('a', class_='s-item__link')['href']
+        except Exception as e:
+            item_link = 'None'
+        print(item_link,"\n")
+        print()
 
-# call the functions
+# call the function    
 if __name__ == '__main__':
-    s = MultiThreadScraper("https://www.learnpython.org/")           # link for crawling
-    s.run_scraper()
+    try:
+        crawler()
+    except:
+        print("\nError")
+
