a980f6fdfa5c6a212054dad50c53940c8c9b4558
basketcase03 <muskankhajuria03@gmail.com>
2020-09-11 22:24:01 +0530

    Added Scraper

---
+++ Python/Scrape_Twitter/Scrape_Twitter.py
@@ -0,0 +1,31 @@
+import tweepy
+import pymongo
+from pymongo import MongoClient
+client = MongoClient()
+
+
+#Enter Tweepy credentials
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+
+#Edit according to needs, here is localhost
+client = MongoClient('localhost', 27017)
+db = client['scrapped_tweets']
+tweets = db.tweets
+
+
+#Change to the number of tweets required
+MAX_TWEETS = 5000
+
+#Change q to get relevant tag
+for tweet in tweepy.Cursor(api.search, q='#blacklivesmatter', rpp=100).items(MAX_TWEETS):
+    print(tweet)
+    result = tweets.insert_one(tweet._json)
+print("Scrapped!")
