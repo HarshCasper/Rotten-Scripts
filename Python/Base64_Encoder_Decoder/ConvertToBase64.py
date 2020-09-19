682bcac3a0041099632b5a397f6f8195a3a6df73
narayan2111 <narayandwivedi076@gmail.com>
2020-09-19 20:43:56 +0530

    Added Argument Parser

--- Python/Base64_Encoder_Decoder/ConvertToBase64.py
+++ Python/Base64_Encoder_Decoder/ConvertToBase64.py
@@ -3,24 +3,30 @@
 
 # importing base64
 
-def ConvertToBase64():
-	import base64
+import base64
+import argparse
+parser = argparse.ArgumentParser(description='String Encode and Decode')
+parser.add_argument('string', help='Enter String to encode')
+args = parser.parse_args()
 
-	string = input("Enter string to encode:")
 
-	# encoding the string to base64
-	string = base64.b64encode(string.encode('utf-8'))
-	print(string)
+def ConvertToBase64(string):
 
-	# decoding the string to base64
-	print("Want to decode(Y/N):")
-	choice = input()
-	if(choice == 'y' or choice == 'Y'):
-	    print("Decoded String:")
+    # encoding the string to base64
+    string = base64.b64encode(string.encode('utf-8'))
+    print(string)
 
-	    #decoding the given string
-	    string = base64.b64decode(string).decode('utf-8')
-	    print(string)
-	else:
-	    print("String Encoded")
-ConvertToBase64()
+    # decoding the string to base64
+    print("Want to decode(Y/N):")
+    choice = input()
+    if(choice == 'y' or choice == 'Y'):
+        print("Decoded String:")
+
+        # decoding the given string
+        string = base64.b64decode(string).decode('utf-8')
+        print(string)
+    else:
+        print("String Encoded")
+
+if __name__ == '__main__':
+    ConvertToBase64(args.string)
