3666ebd2cf1b47941f2cb0e1c22d4502b5d63af6
Rutuja Dhanawade <53823042+rutujadhanawade@users.noreply.github.com>
2020-09-20 15:15:22 +0530

    modified code

--- Python/pdf_tables_extractor/PDFtoCSV.py
+++ Python/pdf_tables_extractor/PDFtoCSV.py
@@ -1,20 +1,19 @@
+''' python code to convert pdf to csv
+'''
+import sys
 import tabula
-
 
 def extract_tables(path, number_pages):
     tables = tabula.read_pdf(path, multiple_tables=True, pages=number_pages)
     num = 1
-    if len(tables) != 0:
+    if tables:
         for table in tables:
             print("Saving file ...")
             table.to_csv(f'Table-{num}.csv')
             num += 1
-        print("created a csv file!")
+        print(f"created a csv file!'Table-{num}.csv'")
     else:
         print("No tables found !")
 
 
-path = input("Enter the path to pdf file")
-n = input("Enter number of pages to extract")
-extract_tables(path, n)
-
+extract_tables(sys.argv[1], sys.argv[2])
