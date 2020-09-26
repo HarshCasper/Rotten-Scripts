3ed935a830b6614d923ce4b0673b6f55250e57fb
Kaustubh <43691873+kaustubhgupta@users.noreply.github.com>
2020-09-26 12:59:32 +0530

    Update app.py
    Comments added

--- Python/Git_repo_size_contributors/app.py
+++ Python/Git_repo_size_contributors/app.py
@@ -6,24 +6,24 @@ from tqdm import tqdm
 
 
 def search(username, repo_name, token):
-    headers = {'Authorization': 'token %s' % token}
+    headers = {'Authorization': 'token %s' % token}  # Header required for authentication as without this limit is 60 request per hour and with this 5000 per hour.
     size_link = f"https://api.github.com/repos/{username}/{repo_name}"
 
     size_request = requests.get(size_link)
     data_received = json.loads(size_request.content)
-    size = data_received["size"]/1024
+    size = data_received["size"]/1024  # Converting Kilobytes to Megabytes
 
     data = {}
     data['Size of Repo (MB)'] = size
 
     page_no = 1
     progress_bar = tqdm(unit=' Page')
-    while True:
+    while True:  # This loop will run untill it receives a blank page with no contributors
         contrib_link = size_link + f"/contributors?per_page=100&page={page_no}"
         contrib_request = requests.get(contrib_link, headers=headers)
         contributors = json.loads(contrib_request.content)
         if len(contributors)!=0:
-            for contributor in contributors:
+            for contributor in contributors:  # extracting contributor username and github link from JSON received
                 if contributor["type"] == "User":
                     data[contributor["login"]] = contributor["html_url"]
         else:
@@ -31,7 +31,7 @@ def search(username, repo_name, token):
         progress_bar.update(page_no)
         page_no += 1
     try:
-        df = pd.DataFrame(data=data.values(), index=data.keys())
+        df = pd.DataFrame(data=data.values(), index=data.keys())  # Saving the details received in CSV file
         df.to_csv("Data.csv")
     except:
         print('No contributors found!' + '\n' + f'Size of Repo: {size}')
