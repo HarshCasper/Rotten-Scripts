3625ad0ddb3051cc6c42ee51fc29875bcc507c45
sujoypal51 <70856336+sujoypal51@users.noreply.github.com>
2020-10-14 22:28:14 +0530

    Changed the approach
    I used only two modules here and made a random password generator by user input.

--- Python/Random_Password_Generator/password_generator.py
+++ Python/Random_Password_Generator/password_generator.py
@@ -1,30 +1,19 @@
-from secrets import choice
-from sys import argv
-from string import ascii_lowercase, ascii_uppercase
+import string
+import random
 
-
-def generate_password(password_size):
-    # Creates one big string with all available characters
-    # ' !"#...0123...ABC...xyz'
-    numbers = '0123456789'
-    symbols = ' !"#$%&\'()*+,-./:;<=>?@[\\^_`{|}'
-    available_characters = numbers + symbols + ascii_lowercase + ascii_uppercase
-
-    # Iterate password_size times, each time taking a new random character
-    # from our list of available_characters
-    password = ''
-    for i in range(password_size):
-        password += choice(available_characters)
-
-    return password
-
-
-if __name__ == '__main__':
-    # Tries to generate a password with argv[1] length. Remember that
-    # argv[0] is the program name (password_generator) and argv[1] is
-    # the length of the password which the user wants to generate.
-    if len(argv) != 2:
-        print('Usage: python3 password_generator.py password_length')
-        print('Example: python3 password_generator.py 20')
-    else:
-        print(generate_password(int(argv[1])))
+if __name__ == "__main__":
+    # All available LOWERCASE,UPPERCASE,DIGITS AND PUNCTUATION.
+    s1=string.ascii_lowercase
+    s2=string.ascii_uppercase
+    s3=string.digits
+    s4=string.punctuation
+    #Take input of password length from user
+    plen=int(input("Enter Password Length\n"))
+    #Make a empty list and extend all elements in the list
+    s=[]
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    #Random elements pick from the list S
+    random.shuffle(s)
+    print("".join(s[0:plen]))
