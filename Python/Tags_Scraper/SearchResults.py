93c351e3c41bf0d7c8a2c9721fb06a75e4c76894
Tushar Goswami <tushargoswami1510@gmail.com>
2020-10-29 01:03:58 +0530

    Done changes according to the suggestions

--- Python/Tags_Scraper/SearchResults.py
+++ Python/Tags_Scraper/SearchResults.py
@@ -1,44 +1,47 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import os
-from Fetch_Tags import FetchTags
 from URL_generator import GURL
 
 def FetchURLs(query):
-	directory = os.getcwd()
 
-	if os.path.exists(directory + r"/links_fetched.txt"):
-		os.remove(directory + r"/links_fetched.txt")
+    """
+    FetchURLs method takes in the query from the main method , connects to google and search
+    for the query , scrapes the URLs of the search results and returns the list of the 
+    Links.
+    """
+    directory = os.getcwd()
 
-	obj = GURL(query)
-	obj.generate()
-	url = obj.get_url()
+    if os.path.exists(directory + r"/links_fetched.txt"):
+        os.remove(directory + r"/links_fetched.txt")
 
-	fp = open(directory + r"/links_fetched.txt", 'w')
-	fp.write("URL Fetched : {}\n\n".format(url))
+    obj = GURL(query)
+    obj.generate()
+    url = obj.get_url()
 
-	usr_agent = {
-	        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-	                      'Chrome/61.0.3163.100 Safari/537.36'}
+    links_file = open(directory + r"/links_fetched.txt", 'w')
+    links_file.write("URL Fetched : {}\n\n".format(url))
 
-
-	req = requests.get(url, headers = usr_agent)
-	req.raise_for_status()
-	soup = BeautifulSoup(req.text, 'html.parser')
+    usr_agent = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/61.0.3163.100 Safari/537.36'}
 
 
-	results = []
-	i =1
+    req = requests.get(url, headers=usr_agent)
+    req.raise_for_status()
+    soup = BeautifulSoup(req.text, 'html.parser')
 
-	for result in soup.find_all('div', attrs={'class': 'g'}):
-	    link = result.find('a', href=True)
-	    title = link.find('h3')
-	    if link and title :
-	    	results.append(link['href'])
-	    	fp.write("{}) Link : {}\n\n".format(i, link['href']))
-	    	fp.write("\n\n________________________________________\n\n")
-	    	i+=1
-	
-	fp.close()
 
-	return results
+    results = []
+    i = 1
+
+    for result in soup.find_all('div', attrs={'class': 'g'}):
+        link = result.find('a', href=True)
+        title = link.find('h3')
+        if link and title:
+            results.append(link['href'])
+            links_file.write("{}) Link : {}\n\n".format(i, link['href']))
+            links_file.write("\n\n________________________________________\n\n")
+            i += 1
+    links_file.close()
+    return results
