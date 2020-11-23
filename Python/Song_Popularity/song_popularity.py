b1811f50971c631bc9c1c3aad945cfb93f26998a
vybhav72954 <vybhavchaturvedi@gmail.com>
2020-11-23 09:47:33 +0530

    Removed uaeless info

--- Python/Song_Popularity/song_popularity.py
+++ Python/Song_Popularity/song_popularity.py
@@ -1,6 +1,6 @@
 import lyricsgenius
 # Feed your Client Access Token
-genius = lyricsgenius.Genius("9ty1rExjjFq3wXSm404uxGX9Pf0dp7K5H6FiJzr3LxW5XS7xdH7S_iG7g5H4NTGb")
+genius = lyricsgenius.Genius("___")
 
 # Feed the artist name, and the number of song
 artist = genius.search_artist("Coldplay", max_songs=3, sort="popularity")
