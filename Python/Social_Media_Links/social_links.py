dab1e450806094d388616947c5726ff2a10bf6d6
vybhav72954 <vybhavchaturvedi@gmail.com>
2020-11-23 10:10:31 +0530

    Social Media Links

---
+++ Python/Social_Media_Links/social_links.py
@@ -0,0 +1,10 @@
+import requests
+from html_to_etree import parse_html_bytes
+from extract_social_media import find_links_tree
+
+res = requests.get('https://github.com/HarshCasper/Rotten-Scripts')
+
+tree = parse_html_bytes(res.content, res.headers.get('content-type'))
+
+links = set(find_links_tree(tree))
+print(links)
