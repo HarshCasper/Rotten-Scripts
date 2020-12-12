15e92a4beccd14d8515d0a4e92ef08516ccf7b4f
Manish Kumar <40482920+ghostmk@users.noreply.github.com>
2020-12-13 02:55:47 +0530

    Create autoclkr.py

---
+++ Python/Autoclicker/autoclkr.py
@@ -0,0 +1,53 @@
+import pyautogui
+from pynput.keyboard import *
+
+#  ======== settings ========
+delay = 1  # in seconds
+resume_key = Key.f1
+pause_key = Key.f2
+exit_key = Key.esc
+#  ==========================
+
+pause = True
+running = True
+
+def on_press(key):
+    global running, pause
+
+    if key == resume_key:
+        pause = False
+        print("[Resumed]")
+    elif key == pause_key:
+        pause = True
+        print("[Paused]")
+    elif key == exit_key:
+        running = False
+        print("[Exit]")
+
+
+def display_controls():
+    print("// AutoClicker by iSayChris")
+    print("// - Settings: ")
+    print("\t delay = " + str(delay) + ' sec' + '\n')
+    print("// - Controls:")
+    print("\t F1 = Resume")
+    print("\t F2 = Pause")
+    print("\t F3 = Exit")
+    print("-----------------------------------------------------")
+    print('Press F1 to start ...')
+
+
+def main():
+    lis = Listener(on_press=on_press)
+    lis.start()
+
+    display_controls()
+    while running:
+        if not pause:
+            pyautogui.click(pyautogui.position())
+            pyautogui.PAUSE = delay
+    lis.stop()
+
+
+if __name__ == "__main__":
+    main()
