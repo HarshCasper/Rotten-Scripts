fe750d85b5c96863e6cc12e911c3e3a6b1d0b53d
rahulhgdev <rahul.gupta7749@gmail.com>
2020-12-16 21:10:03 +0530

    Added Quote-of-the-day JavaScript Code

---
+++ JavaScript/Quote of the day/quotes.js
@@ -0,0 +1,28 @@
+//Using array to hold the value of quote
+const arrayOfQuotes = [
+    {'author': 'Oscar Wilde', 
+     'quote': 'Be yourself; everyone else is already taken.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Wayne Gretzy', 
+     'quote': 'You miss 100% of the shots you don\'t take.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    },
+];
+
+function generateQuote(){
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
+    document.querySelector('#quoteOutput').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
+    
+}
