105610331d8d9fbcc34c45a69b36e92133da65d3
Mohta Rahul Suresh <60074958+Rahul555-droid@users.noreply.github.com>
2020-12-20 13:36:52 +0530

    Update pdf_reorder_.py

--- Python/pdf_reorder/pdf_reorder_.py
+++ Python/pdf_reorder/pdf_reorder_.py
@@ -5,14 +5,15 @@ the order required by reorder(path,dic) function
 It takes one argument which is page length of pdf 'n'
 """
 
+
 def input_and_parse(n):
     print("enter the current page and the page you want it to be on seperate values by a comma ',' \n")
     """
     store the input in a list and then convert the input string into
     using map function to convert the data in lists into int values
     """
-    lst = list(map(lambda x: [int(x[0]), int(x[1])], 
-               [input().split(',') for _ in range(n)]))
+    lst = list(map(lambda x: [int(x[0]), int(x[1])],
+                   [input().split(',') for _ in range(n)]))
     # Swapping the position of the lst values to better parse it in dictionary
     lst = [[x[1], x[0]] for x in lst]
     lst.sort(key=lambda x: x[0])
@@ -20,17 +21,20 @@ def input_and_parse(n):
     # now I have sorted the dic to the required needs of reorder function
     return dic
 
+
 """
 The function reorder takes two arguments path and dic
 path is the path of the source pdf file which is in wrong
 order
 """
+
+
 def reorder(path, dic):
     # create a pdf object using PdfReader that could be read
     pdf_obj = PdfReader(path)
     # pdf_obj.pages attribute gives the length of the pages in pdf
     total_pages = len(pdf_obj.pages)
-    print("Total Pages in PDF are:",total_pages)
+    print("Total Pages in PDF are:", total_pages)
     # Initialising the writer object using the PdfWriter class,from this we would create a new modified Pdf
     writer = PdfWriter()
 
