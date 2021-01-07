4a66f5b1161663f602aff545b8a723848f1e727a
Pankaj <pankaj28988@gmail.com>
2021-01-07 22:42:00 +0530

    Added Location based on IP Address Script

--- Python/Get_location_based_on_IP_Address/locationip.py
+++ Python/Get_location_based_on_IP_Address/locationip.py
@@ -1,28 +1,3 @@
-#import geocoder
-# = geocoder.ipinfo('42.106.238.166')
-#print(g.latlng)
-
-#from requests import get
-#import json
-
-#url = 'http://ipinfo.io/json'
-#response = get(url)
-#data = json.loads(response.text)
-
-#city = data['city']
-#region = data['region']
-#country = data['country']
-#postal = data['postal']
-#timezone = data['timezone']
-#ip = data['ip']
-
-#print("ip:", ip)
-#print("city:", city)
-#print("region:", region),
-#print("country:", country)
-#print("postal:", postal)
-#print("timezone:", timezone)
-
 from ipregistry import IpregistryClient
 
 client = IpregistryClient("tryout")  
