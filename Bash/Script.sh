b32300beeb27a985a29ca276c1898f0574c0f470
Himanshu <65963997+himanshu007-creator@users.noreply.github.com>
2021-01-19 18:59:55 +0530

    Automating your socials
    This a simple bash script that will help to set a good impression on anyone. Simply replace the comments with the prompted information and when you will run it, it will look really good. It will seem like that you are communicating through computer and sharing your socials.

---
+++ Bash/Script.sh
@@ -0,0 +1,16 @@
+  
+echo HI , Im am #your name.
+sleep 1
+echo i love to learn new things and i believe everything is possible
+sleep 0.8
+echo I love open-source. Lets get connected
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+echo HERE YOU GO!!
+sleep 0.5
+explorer #url-1
+explorer #url-2
