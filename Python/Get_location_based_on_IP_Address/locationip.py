cbc4da77e2be090609c4f64365f6973be0caee55
Pankaj <pankaj28988@gmail.com>
2021-01-23 10:45:07 +0530

    Updated locationip.py

--- Python/Get_location_based_on_IP_Address/locationip.py
+++ Python/Get_location_based_on_IP_Address/locationip.py
@@ -1,12 +1,5 @@
 from ipregistry import IpregistryClient
 
-<<<<<<< HEAD
 user = IpregistryClient("tryout")  
 ipInfo = user.lookup() 
 print(ipInfo)
-=======
-client = IpregistryClient("tryout")  
-ipInfo = client.lookup() 
-print(ipInfo)
-
->>>>>>> 05eeba24e494207e84e09bf36b8326547504878a
