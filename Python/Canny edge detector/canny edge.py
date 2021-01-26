48ec0f973ce4827baff6619d1d59c885b591d265
Vaishnavi Saxena <1900520400064@ietlucknow.ac.in>
2021-01-26 17:27:07 +0530

    Update canny edge.py

--- Python/Canny edge detector/canny edge.py
+++ Python/Canny edge detector/canny edge.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('C:/Users/hp/Downloads/messi.jpg',0)
+img = cv2.imread('messi.jpg',0)
 edges = cv2.Canny(img,100,200)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
