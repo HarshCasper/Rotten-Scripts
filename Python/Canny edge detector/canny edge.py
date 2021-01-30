cb0933fb9c909e9ac95942929a0e8cec3495e900
Vaishnavi Saxena <1900520400064@ietlucknow.ac.in>
2021-01-30 09:17:55 +0530

    Update canny edge.py

--- Python/Canny edge detector/canny edge.py
+++ Python/Canny edge detector/canny edge.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import tkinter as tk 
+from tkinter import filedialog 
+root = tk.Tk() 
+root.withdraw() 
+file_path = filedialog.askopenfilename()
 
-img = cv2.imread('messi.jpg',0)
+
+img = cv2.imread(file_path,0)
 edges = cv2.Canny(img,100,200)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
@@ -11,3 +17,4 @@ plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
 plt.show()
+
