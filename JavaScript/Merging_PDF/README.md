6ebe838384b6d42efb7b71d3ae8f118a560886d4
Kumaran KM <kumaranmohan1106@gmail.com>
2021-02-07 02:33:03 +0530

    Added js code to merge pdf

---
+++ JavaScript/Merging_PDF/README.md
@@ -0,0 +1,10 @@
+# Merging PDF
+
+A Javascript Code that merge two pdf into single pdf with the help of pdf-lib library. First, this code will load pdf from the directory and then it creates new documents to add these loaded pdf into it.
+
+Before merging:
+![image](beforemerge.png)
+
+After merging:
+![image](Aftermerge.png)
+
---
+++ JavaScript/Merging_PDF/mergepdf.js
@@ -0,0 +1,32 @@
+const { PDFDocument }= require('pdf-lib');
+const fs= require('fs');
+
+
+//For catching errors
+run().catch(err=>console.log(err));
+
+
+async function run(){
+    //Loading the pdf which we need to convert
+    const pdf1 = await PDFDocument.load(fs.readFileSync('1.pdf'));
+    const pdf2 = await PDFDocument.load(fs.readFileSync('2.pdf'));
+
+    //creating a new empty document
+    const doc = await PDFDocument.create();
+
+    //Adding first pdf to the empty doc
+    const first = await doc.copyPages(pdf1, pdf1.getPageIndices());
+    for (const page of first){
+        doc.addPage(page);
+    }
+
+    //Adding first pdf to the empty doc
+    const second = await doc.copyPages(pdf2, pdf2.getPageIndices());
+    for (const page of second){
+        doc.addPage(page);
+    }
+
+    //creating a name for merged pdf
+    fs.writeFileSync('./rottenscript.pdf',await doc.save());
+
+}
