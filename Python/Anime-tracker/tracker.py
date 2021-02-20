4fec603b798fa0a62b0432bf4ba694471a00c1d8
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-02-20 17:50:22 +0530

    Update tracker.py

--- Python/Anime-tracker/tracker.py
+++ Python/Anime-tracker/tracker.py
@@ -4,22 +4,19 @@ try:
     from bs4 import BeautifulSoup
     import urllib.parse as parse
     import re
-    
-
 except ImportError:
     print('Some modules are not installed! ')
 
-# mainly bs4 lib is used for extracting html from web pages
-
+# BS4 Library is being used to extract HTML from the Webpages
 def details(soup):
-    # selecting div with class pure...
+    # selecting div with class pure
     info = soup.find('div', {'class': 'pure-1 md-3-5'})  
     # now extracting the text for p tag of the div 
-    print("\nAbout  the Anime : \n", "\t\t", info.find('p').getText(), "\n")                              
+    print("\nAbout the Anime : \n", "\t\t", info.find('p').getText(), "\n")                              
 
     total_episodes = soup.find('div', {'class': 'pure-1 md-1-5'})
     print("\nTotal number of episodes :\t",
-           # usimg regex for only selecting numbers
+           # using regex for only selecting numbers
           re.sub("[^0-9]", "", total_episodes.find('span').getText()))     
 
     Active_years = soup.find('span', {'class': 'iconYear'})
@@ -30,8 +27,6 @@ def details(soup):
     print("Rating : ", rating.find('span').getText())
 
     tags = soup.find('div', {'class': 'tags'})
-    # print("Tags : ", tags.find('ul').getText())
-
     list = []
     for _ in range(4):
         list.append(tags.find('ul').getText())
@@ -39,28 +34,24 @@ def details(soup):
     print("\nTags : \n")
     print((list[0].replace("\n", "  ")))
 
-
 def entry():
     print("\nType complete name>>\n")
     anime_name = input(
         "[+] Enter the name of the Anime : ").strip().title().replace(" ", "-")
-
+    
     print("\n")
     print(anime_name)
-
     search_url = ("https://www.anime-planet.com/anime/" + anime_name)
     source_code = requests.get(search_url)
     content = source_code.content
     global soup
     # to parse the selectd HTML
     soup = BeautifulSoup(content, features="html.parser")    
-    # print(soup.prettify)
 
     try:
         details(soup)
     except AttributeError:
         print("Anime info not found")
-
 
 if __name__ == "__main__":
     entry()
