0466919f41fea61a712b8316e5b63b1d1777ea11
nimit2801 <mnimitsavant@outlook.com>
2021-02-24 23:04:32 +0530

    updated script with pylint

--- Python/Subreddit_Image_Scrapper/script.py
+++ Python/Subreddit_Image_Scrapper/script.py
@@ -1,48 +1,47 @@
+
 # import modules
-import requests
-import json
 import os
-
-
-# Function for downloading images
+import requests
 
 
 def download_file(url_, i, limit, subreddit):
-    r = requests.get(url_, stream=True)
-    if r.ok:
+    """
+    Function for downloading images
+    """
+    request_sub = requests.get(url_, stream=True)
+    if request_sub.ok:
         with open(f'Subreddit/{subreddit}/file{i}.png', 'wb') as file:
-            for chunk in r.iter_content(1024 * 100):
+            for chunk in request_sub.iter_content(1024 * 100):
                 file.write(chunk)
         print(f'[{i}/{limit}] done')
     else:
-        print(f'[{i}/{limit}] {r.status_code} whoops: Was not downloaded')
-    # [#############]
-
-# main function for the script
+        print(f'[{i}/{limit}] {request_sub.status_code} whoops: Was not downloaded')
 
 
 def main():
+    """
+    main function for the script
+    """
     subreddit = input("Please enter the subreddit you want to memes from: ")
     sort = input(
         "Please input sorting method here: (options: 'Top', 'Hot', 'new')")
-    limit = eval(input("Number of memes: "))
+    limit = input("Number of memes: ")
     payload = {'sort': sort, 'limit': limit}
     print(payload)
     headers = {'user-agent': 'my-app/0.0.1'}
     # request for getting the urls of the subreddit
     url = f'https://www.reddit.com/r/{subreddit}.json'
-    r = requests.get(url, params=payload, headers=headers)
+    request_sub = requests.get(url, params=payload, headers=headers)
 
-    print(r.url)
-    print(r.status_code)
-    print(r.headers['content-type'])
-    data1 = r.json()
+    print(request_sub.url)
+    print(request_sub.status_code)
+    print(request_sub.headers['content-type'])
+    data1 = request_sub.json()
     i = 0
     limit = int(data1["data"]["dist"])-2
     if not os.path.exists(f'Subreddit/{subreddit}'):
         os.makedirs(f'Subreddit/{subreddit}')
     for i in range(0, limit):
-        # print(data1["data"]["children"][i]["data"]["url"]) <-- Name of the URL downloading images
         download_file(data1["data"]["children"][i]
                       ["data"]["url"], i, limit, subreddit)
         i = i + 1
