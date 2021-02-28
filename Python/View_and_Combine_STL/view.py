4b98d1a31ed8980caa61bce68f6a6829da516da0
Archan Ghosh <gharchan@gmail.com>
2021-02-28 10:51:27 +0530

    Update view.py

--- Python/View_and_Combine_STL/view.py
+++ Python/View_and_Combine_STL/view.py
@@ -17,9 +17,10 @@ def view_stl():
 	view_mesh = mesh.Mesh.from_file(file_dir)
 	axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 	
-	#Here we calculated the total number of flat vertices, that are present in a 3D object and then scale it according to the total Figsize of the plot
-	#Autoscale allows us to fit the object within the constraints of a PyPlot Graph, and also fixes the object at 0,0,0 coordinates which 
-	#can help us understand the true dimensions of the object in inches
+	''' Here we calculated the total number of flat vertices, that are present in a 3D object and then scale it according to the total Figsize of the plot
+	autoscale allows us to fit the object within the constraints of a PyPlot Graph, and also fixes the object at 0,0,0 coordinates which can help us understand the 
+	true dimensions of the object in inches
+	'''
 	scale = view_mesh.points.flatten()
 	axes.auto_scale_xyz(scale, scale, scale)
 
