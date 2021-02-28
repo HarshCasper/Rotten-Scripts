69c928c46b4b01eb50402b28c39f46f9c8166915
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-02-28 18:02:59 +0530

    Update tracker.py

--- Python/BITCOIN-price-tracker/tracker.py
+++ Python/BITCOIN-price-tracker/tracker.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-
 # create a function to get price of cryptocurrency
 def get_latest_crypto_price(coin):
     url = 'https://www.google.com/search?q='+(coin)+'price'
@@ -9,9 +8,8 @@ def get_latest_crypto_price(coin):
     HTML = requests.get(url)
     # Parsse the HTML
     soup = BeautifulSoup(HTML.text, 'html.parser')
-    # find the current price
+    # finding the current price
     texti = soup.find('div', attrs={'class':'BNeawe iBp4i AP7Wnd'}).find({'div':'BNeawe iBp4i AP7Wnd'}).text
     return texti
-
 price = get_latest_crypto_price('bitcoin')
 print('BITCOIN price : ' + price)
