ede131488189ebdb89ecb11553dd5a07d3d61ccb
pi-squared-4 <aman.44.nitr@gmail.com>
2021-03-10 10:59:51 +0530

    Fixed Readme, made required changes.

--- Python/Background_Remover/README.md
+++ Python/Background_Remover/README.md
@@ -1,2 +1,36 @@
-**HELLO**
+## Backgraound Remover ##
+
+- This is simple background remover script that intends to remove background from a still frame.
+- This uses python with various libraries like openCV and numpy.
+
+## How to Setup ##
+
+- Clone the folder to your local repository.
+- Install libraries like openCV using command *pip install opencv-python*. 
+- The *main.py* file does all the work. 
+- Change path of the image on line number    to the path of image you want to remove background of.
+- Run the program from CLI using *python main.py*.
+
+## How does this work ## 
+
+- It reads the input.
+- Converts it into grey.
+- Thresholds it and inverts it as mask.
+- Optionally apply morphology to clean up any extraneous spots
+- Anti-alias the edges.
+- Convert a copy of the input to BGRA and insert the mask as the alpha channel.
+
+## Parameters and customizations ##
+
+- BLUR - To change the amount of opacity you want to cover while masking the input.
+- MASK_COLOR - To change the background of the output to your desired color. The color format used there is BGR.
+
+
+## What you might see ##
+
+- Output and Input Image. 
+![Image]('https://imgur.com/gallery/IDRm79h')
+
+## Author ##
+- [piSquared]('https://github.com/pi-squared-4')  
 
--- Python/Background_Remover/main.py
+++ Python/Background_Remover/main.py
@@ -7,10 +7,10 @@ CANNY_THRESH_1 = 10
 CANNY_THRESH_2 = 200
 MASK_DILATE_ITER = 10
 MASK_ERODE_ITER = 10
-MASK_COLOR = (0.0,0.0,0.0) # In BGR format
 
+# In BGR format
+MASK_COLOR = (0.0,0.0,0.0)
 
-#== Processing =======================================================================
 
 #-- Read image -----------------------------------------------------------------------
 img = cv2.imread('person.png')
@@ -26,7 +26,7 @@ contour_info = []
 contours, __ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 # Previously, for a previous version of cv2, this line was:
 #  contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-# Thanks to notes from commenters, I've updated the code but left this note
+
 for c in contours:
     contour_info.append((
         c,
@@ -38,6 +38,7 @@ max_contour = contour_info[0]
 
 #-- Create empty mask, draw filled polygon on it corresponding to largest contour ----
 # Mask is black, polygon is white
+
 mask = np.zeros(edges.shape)
 cv2.fillConvexPoly(mask, max_contour[0], (255))
 
@@ -45,22 +46,30 @@ cv2.fillConvexPoly(mask, max_contour[0], (255))
 mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
 mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
 mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
-mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
+
+# Create 3-channel alpha mask
+mask_stack = np.dstack([mask]*3)
 
 
 
 #-- Blend masked img into MASK_COLOR background --------------------------------------
-mask_stack  = mask_stack.astype('float32') / 255.0          # Use float matrices,
-img         = img.astype('float32') / 255.0                 #  for easy blending
+# Use float matrices,
+mask_stack  = mask_stack.astype('float32') / 255.0
+#  for easy blending
+img         = img.astype('float32') / 255.0
 
-masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR) # Blend
-masked = (masked * 255).astype('uint8')                     # Convert back to 8-bit
+# Blend
+masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR)
+# Convert back to 8-bit
+masked = (masked * 255).astype('uint8')
 
 c_red, c_green, c_blue = cv2.split(img)
 img_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
 
-cv2.imshow('img', masked)                                   # Display
+# Display
+cv2.imshow('img', masked)
 cv2.imshow('img2' , img_a)
 cv2.waitKey()
 
+# If you want to save the output --------------------------------------------
 #cv2.imwrite('C:/Temp/person-masked.jpg', masked)
