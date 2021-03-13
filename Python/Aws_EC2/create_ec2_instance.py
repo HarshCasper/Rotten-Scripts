3ffc9602b3acb5f83e1a5093c0c98d44c056fd6b
sakar97 <sakar.mehra@gslab.com>
2021-03-13 23:56:10 +0530

    fix sanity

--- Python/Aws_EC2/create_ec2_instance.py
+++ Python/Aws_EC2/create_ec2_instance.py
@@ -2,12 +2,13 @@ import boto3
 
 
 class ec2_instance:
-    '''This class will create ec2 instance'''
+    '''This class will create ec2 instance.'''
+
     def __init__(self, ec2Instance: str) -> None:
         self.ec2Instance = ec2Instance
 
     def key_pair(self):
-        '''This Function definition will create a keypair for ec2'''
+        '''This Function definition will create a keypair for ec2.'''
         ec2 = boto3.resource('ec2')
         print(self.ec2Instance)
         # create a file to store the key locally
@@ -18,10 +19,10 @@ class ec2_instance:
 
         # capture the key and store it in a file
         KeyPairOut = str(key_pair.key_material)
-
+        outfile.write(KeyPairOut)
 
     def create_ec2(self):
-        '''This will create an ec2 instance'''
+        '''This will create an ec2 instance.'''
         try:
             # create a new EC2 instance
             ec2 = boto3.resource('ec2')
@@ -32,8 +33,8 @@ class ec2_instance:
                 InstanceType='t1.micro',
                 KeyName='ec42-keypair'
             )
-        except Exception as e:
-            print(e.with_traceback)
+        except Exception as err:
+            print("Error {0}".format(err))
         else:
             print("EC2 Instance created", instances)
 
