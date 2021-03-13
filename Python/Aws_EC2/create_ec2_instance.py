6df34112330f08f22a72018cca7db96173a54d4b
sakar97 <sakar.mehra@gslab.com>
2021-03-13 23:49:16 +0530

    fix sanity

--- Python/Aws_EC2/create_ec2_instance.py
+++ Python/Aws_EC2/create_ec2_instance.py
@@ -1,27 +1,27 @@
 import boto3
-import os
-import datetime
+
 
 class ec2_instance:
+    '''This class will create ec2 instance'''
     def __init__(self, ec2Instance: str) -> None:
         self.ec2Instance = ec2Instance
 
     def key_pair(self):
-        
+        '''This Function definition will create a keypair for ec2'''
         ec2 = boto3.resource('ec2')
-
+        print(self.ec2Instance)
         # create a file to store the key locally
-        outfile = open('ec42-keypair.pem','w')
+        outfile = open('ec42-keypair.pem', 'w')
 
         # call the boto ec2 function to create a key pair
         key_pair = ec2.create_key_pair(KeyName='ec42-keypair')
 
         # capture the key and store it in a file
         KeyPairOut = str(key_pair.key_material)
-        outfile.write(KeyPairOut)
 
 
     def create_ec2(self):
+        '''This will create an ec2 instance'''
         try:
             # create a new EC2 instance
             ec2 = boto3.resource('ec2')
@@ -33,13 +33,13 @@ class ec2_instance:
                 KeyName='ec42-keypair'
             )
         except Exception as e:
-            print(e)
+            print(e.with_traceback)
         else:
-            print("EC2 Instance created")
+            print("EC2 Instance created", instances)
 
 
 if __name__ == "__main__":
     ec2Instance = 'ami-0c7945b4c95c0481c'
-    ob= ec2_instance(ec2Instance)
+    ob = ec2_instance(ec2Instance)
     ob.key_pair()
     ob.create_ec2()
