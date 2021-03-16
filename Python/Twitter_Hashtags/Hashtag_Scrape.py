905ff6ac5b2d32e8f6ec984edc858dd92a6afb2f
Rohini Rao <64858610+RohiniRG@users.noreply.github.com>
2021-03-17 01:41:56 +0530

    Update Hashtag_Scrape.py

--- Python/Twitter_Hashtags/Hashtag_Scrape.py
+++ Python/Twitter_Hashtags/Hashtag_Scrape.py
@@ -4,7 +4,7 @@ import csv
 
 tag = input('Enter a hashtag: #')
 max_count = int(input('Enter maximum number of tweets to be listed: '))
-file_name = input('Enter the name of file to store the tweet info (along with file path): ')
+file_name = input('Enter the name of file to store the tweet info: ')
 
 print('Creating a .csv file regarding the hashtag....')
 
@@ -12,7 +12,7 @@ tweets_list = []
 count = 0
 
 for i in sntweets.TwitterSearchScraper('#'+tag).get_items():
-    tweets_list.append([i.id, i.user.username, i.date, i.content, i.replyCount, i.retweetCount, i.likeCount, i.quoteCount, i.url])
+    tweets_list.append([i.id, i.username, i.date, i.content, i.url])
     count += 1
 
     if count == max_count:
@@ -20,7 +20,7 @@ for i in sntweets.TwitterSearchScraper('#'+tag).get_items():
 
 
 if count != 0:
-    header_list = ['Id', 'Username', 'Date and Time', 'Tweet', 'Reply count', 'Retweet count', 'Like count', 'Quote count', 'Url']
+    header_list = ['Id', 'Username', 'Date and Time', 'Tweet', 'Url']
 
     with open(file_name+'.csv', 'a', encoding='utf-8') as csv_f:
         csv_pointer = csv.writer(csv_f, delimiter=',')
@@ -31,4 +31,3 @@ if count != 0:
 
 else:
     print('No tweets available for this hashtag')
-    
