70279052422b4dca50f866c75abf4f164c98444f
rohinirg <rohinirao2112@gmail.com>
2021-03-24 11:15:36 +0530

    Footballer club info wikipedia scraper for issue #466

---
+++ Python/Wikipedia_Footballer_Scraper/README.md
@@ -0,0 +1,24 @@
+# Wikipedia scraper to get a Football player's club career information  
+
+- Using BeautifulSoup, a python library useful for web scraping, this script helps to scrape Wikipedia to obtain a Football player's club career data.
+
+- In this script, we take the input of the name of any popular Football player from the user and generate the club career details associated with them.
+
+## Setup Instructions
+
+### Requirements
+
+The requirements of the script can be installed by running the following command which will install dependencies including BeautifulSoup and Requests, essential for the code. 
+
+```console
+computer@computer:~$ pip3 install -r requirements.txt
+```
+
+## Working
+
+The input/outputs for the given scripts is as follows: 
+
+![image](https://i.imgur.com/Uo6RAhQ.png)
+
+## Author
+[Rohini Rao](https://github.com/RohiniRG)
---
+++ Python/Wikipedia_Footballer_Scraper/main.py
@@ -0,0 +1,108 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_player_name():
+    """
+    Takes Football player's name input from the user and
+    returns a string suitable to concatenate in the wikipedia URL.
+    """
+
+    player = input('\nEnter the name of the Football player: ').split(' ')
+    player_name = ''
+    for i in player:
+        name = i.capitalize()
+        player_name += name
+        player_name += ' '
+
+    return player_name
+
+
+def get_club_info(headers):
+    """
+    Stores only the information regarding the club career of
+    a player among all the titles in the contents box.
+    """
+
+    # For storing the club data
+    club_data = []
+
+    try:
+        start = None
+        for i in range(len(headers)):
+            each = headers[i].getText()
+            if each == "Club career":
+                start = i + 1
+                break
+
+        while headers[start].getText() != 'International career':
+            club_data.append(headers[start].getText())
+            start += 1
+
+        return club_data
+
+    # In case the name given is not a Football player
+    except TypeError:
+        print('INVALID PLAYER NAME!!')
+        return 0
+
+
+def display_info(club_data):
+    """
+    To display the club career information of player collected.
+    """
+
+    for item in club_data:
+        if ':' in item:
+            print(f'    {item}')
+        else:
+            print(f'\nCLUB NAME: {item}')
+
+
+def main():
+    """
+    Starts the execution of the script and with Requests module, establishes
+    connection through the Wikipedia URL, to scrape information.
+    """
+
+    print('\nWELCOME!!!HERE YOU CAN GET THE CLUB CAREER INFORMATION'
+          ' REGARDING YOUR FAVOURITE FOOTBALL PLAYER FROM WIKIPEDIA!')
+    while True:
+        name = get_player_name()
+
+        # Concatenating the Football players name to the wikipedia URL
+        # to send a GET request
+        req = requests.get('https://en.wikipedia.org/wiki/' + name)
+
+        if req.status_code == 200:
+
+            # BeautifulSoup object for parsing through the html text
+            soup = BeautifulSoup(req.text, 'html.parser')
+
+            # Searching for the content box with css selector '.mw-headline'
+            titles = soup.select('.mw-headline')
+
+            # collecting info regarding club career only
+            club_info = get_club_info(titles)
+
+            if club_info:
+                # For presenting the collected information
+                display_info(club_info)
+
+            key = input('\nPress "y" to continue or any other key to quit: ')\
+                .lower()
+            if key != 'y':
+                break
+
+        else:
+            print('URL NOT FOUND!!')
+
+            key = input('\nPress "y" to continue or any other key to quit: ')\
+                .lower()
+            if key != 'y':
+                break
+
+
+if __name__ == '__main__':
+    main()
+
