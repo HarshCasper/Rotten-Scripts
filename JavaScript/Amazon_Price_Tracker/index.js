ac9819ea21e5f48eb062a00a130c99d17f95ae19
haarsh01 <harshkaushik717@gmail.com>
2021-03-30 20:21:57 +0530

    Added Amazon price tracker

---
+++ JavaScript/Amazon_Price_Tracker/index.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cron = require('node-cron');
+const nodemailer = require('nodemailer')
+const fetchPrice = async(url, targetPrice) =>{
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const priceRext = $('#priceblock_ourprice').text();
+    const price = parseFloat(priceText.replace('$',''));
+    if(targetPrice >= price){
+        sendEmail(url,price);
+    }else{
+        console.log('too expensive');
+    }
+}
+const sendEmail = async (url, price) => {
+    const testAccount = await nodemailer.createTestAccount();
+    const transport = nodemailer.createTransport({
+        host:'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth:{
+            user: testAccount.user,
+            pass:testAccount.pass,
+        }
+
+    });
+const info = await transport.sendMail({
+    from:'"UI Infinity" <test@test.com>',
+    to:'test@test.com',
+    subject:'amazon watcher',
+    text:`${price} - ${url}`,
+    html:`<p>${price}</p><p>${url}</p>`,
+});
+console.log(nodemailer.getTestMessageUrl(info));
+}
+
+const watchPrice = (priceTaget, url, schedule = '*/5 * * * * *') => {
+    cron.schedule(schedule, () => fetchPrice(url, priceTaget));
+};
+
+
+watchPrice( 53.99,
+    'https://www.amazon.com/Toshiba-HDTB420XK3AA-Canvio-Portable-External/dp/B079D3D8NR/ref=sr_1_6?dchild=1&keywords=hardisk&qid=1617019368&sr=8-6',
+
+
+);
