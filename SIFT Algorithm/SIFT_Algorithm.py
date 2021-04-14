dc60ea207e4efd3be4bb5ecb82cf025b2e505d3c
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2021-04-15 01:39:51 +0530

    Requested changes have been made
    Made a modular function and Displayed the image with dynamic naming.

--- SIFT Algorithm/SIFT_Algorithm.py
+++ SIFT Algorithm/SIFT_Algorithm.py
@@ -7,30 +7,37 @@
 import cv2
 import numpy as np
 
-# Load the image
-path=input("Enter the path of the image: ")
-image = cv2.imread(path)
+def siftAlgorithm():
+    # Load the image
+    path=input("Enter the path of the image: ")
+    image = cv2.imread(path)
 
-# Convert the image to RGB
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Convert the image to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Convert the image to gray scale
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-#Display the input image
-cv2.imshow('Input image.jpg',image)
+    #Display the input image
+    cv2.imshow('Input image.jpg',image)
 
-#Detect keypoints and Create Descriptor
-sift = cv2.xfeatures2d.SIFT_create()
+    #Detect keypoints and Create Descriptor
+    sift = cv2.xfeatures2d.SIFT_create()
 
-kp, dp = sift.detectAndCompute(gray, None)
+    kp, dp = sift.detectAndCompute(gray, None)
 
-img=cv2.drawKeypoints(gray,kp,flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    img=cv2.drawKeypoints(gray,kp,flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-# Print the number of keypoints detected in the image
-print("Number of Keypoints Detected In The Image: ", len(kp))
+    # Print the number of keypoints detected in the image
+    print("Number of Keypoints Detected In The Image: ", len(kp))
+    
+    #Display the final output image
+    image_name = path.split(r'/')
+    image = image_name[-1].split('.')
+    output  = r".\\"+ image[0] + "(_detected).jpg" 
+    cv2_imshow('output',img)
 
-#Display the final output image
-cv2_imshow('sift_keypoints.jpg',img)
+#Driver Code
+siftAlgorithm()
 cv.waitKey(0)
 cv.destroyAllWindows()
