71da200cff1c9b3e0a3d78840362336f58aadd49
jhamadhav <jhamadhav28@gmail.com>
2021-04-15 16:54:14 +0530

    basic db functions added

--- JavaScript/Stargazers/Stargazers.js
+++ JavaScript/Stargazers/Stargazers.js
@@ -1,30 +1,71 @@
-const fetch = require("node-fetch")
-const githubToken = require("./token.js")
+const prompt = require("prompt-sync")();
+const github = require("./githubFunc")
+const db = require("./dbFunc")
+const dbFileLoc = "db.json";
 
-const makeRequest = async (url) => {
-    let res = await fetch(url,
-        {
-            headers: {
-                authorization: `token ${githubToken.token}`
-            }
+const getUserInfo = async (arr) => {
+
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        let userData = await github.getUserData(arr[i])
+        let obj = {
+            "id": arr[i],
+            "name": userData.name,
+            "email": userData.email,
+            "location": userData.location
         }
-    )
-    return res.json()
-}
-
-const getUsers = async (user, repo) => {
-    let users = [], list = [], pageNO = 1;
-    while ((list.length > 0 || pageNO == 1) && pageNO < 10) {
-        list = await makeRequest(`https://api.github.com/repos/${user}/${repo}/stargazers?per_page=100&page=${pageNO}`)
-
-        list = list.map(x => x.login)
-        // console.log(list);
-        users = [...users, ...list];
-        pageNO++;
+        res[i] = obj;
     }
-    // console.log("\nUsers are : ");
-    // console.log(users);
-    console.log(users.length);
-    return users.length;
+    return res;
 }
-getUsers("HarshCasper", "Rotten-scripts")
+
+const addNewData = async (dbData, username, repoName, stargazers) => {
+    stargazers = await getUserInfo(stargazers);
+
+    let data = {
+        "stargazers": stargazers,
+        "unstargazers": [],
+        "time": getDate()
+    }
+    if (!dbData.hasOwnProperty(username)) {
+        dbData[username] = {}
+    }
+    dbData[username][repoName] = data;
+    db.write(dbFileLoc, dbData);
+}
+
+const init = async () => {
+    //these info will be taken through input
+    let username = prompt("Enter Github Username : ");
+    let repoName = prompt("Enter Repo name : ");
+
+    let stargazers = await github.getUsers(username, repoName);
+    stargazers = stargazers.map(user => user.login)
+
+    let dbData = db.read(dbFileLoc);
+    dbData = (dbData === "" || dbData.length == 0) ? "{}" : dbData;
+    dbData = JSON.parse(dbData);
+
+    // if user exits
+    if (dbData.hasOwnProperty(username) && dbData[username].hasOwnProperty(repoName)) {
+
+
+    } else {
+        // if user's repo exists
+        console.log("user doesn't exist");
+        addNewData(dbData, username, repoName, stargazers)
+        console.log("New data added!!");
+    }
+}
+
+const getDate = () => {
+    let d = new Date();
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`
+}
+
+// main function
+console.log(`
+======================
+------Stargazers------
+======================\n`);
+init()
---
+++ JavaScript/Stargazers/githubFunc.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch")
+const githubToken = require("./token.js")
+
+const makeRequest = async (url) => {
+    let res = await fetch(url,
+        {
+            headers: {
+                authorization: `token ${githubToken.token}`
+            }
+        }
+    )
+    return res.json()
+}
+
+const getUsers = async (user, repo) => {
+    let users = [], list = [], pageNO = 1;
+    while ((list.length > 0 || pageNO == 1) && pageNO < 10) {
+        list = await makeRequest(`https://api.github.com/repos/${user}/${repo}/stargazers?per_page=100&page=${pageNO}`)
+
+        users = [...users, ...list];
+        pageNO++;
+    }
+    return users;
+}
+
+const getUserData = async (username) => {
+    let data = await makeRequest(`https://api.github.com/users/${username}`)
+    return await data
+}
+module.exports = { getUsers, getUserData };
