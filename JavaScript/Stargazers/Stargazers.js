df3d705f61ad2401064afd025f8d4e69256a8fe7
jhamadhav <jhamadhav28@gmail.com>
2021-04-16 23:30:51 +0530

    function to print long arrays added

--- JavaScript/Stargazers/Stargazers.js
+++ JavaScript/Stargazers/Stargazers.js
@@ -34,11 +34,27 @@ const addNewData = async (dbData, username, repoName, stargazers) => {
     dbData[username][repoName] = data;
     db.write(dbFileLoc, dbData);
 }
+const getUnstargazers = (dbData, stargazers) => {
 
+}
+
+const printLongArray = (arr, step = 100) => {
+    let start = 0, end = 0;
+    end = Math.min(step, arr.length);
+
+    let userInp = prompt(`Do you want to see list from ${start} to ${end} (y/n): `) || "n"
+    while (userInp == "y" && end <= arr.length && start < end) {
+        subArr = arr.slice(start, end)
+        console.table(subArr, ["name", "email", "location"])
+        start = end;
+        end = Math.min(end + step, arr.length)
+        userInp = prompt(`Do you want to see list from ${start} to ${end} (y/n): `) || "n"
+    }
+}
 const init = async () => {
     //these info will be taken through input
-    let username = prompt("Enter Github Username : ");
-    let repoName = prompt("Enter Repo name : ");
+    let username = "HarshCasper"//prompt("Enter Github Username: ");
+    let repoName = "Rotten-Scripts"//prompt("Enter Repo name : ");
 
     let stargazers = await github.getUsers(username, repoName);
     if (stargazers.message) {
@@ -53,12 +69,23 @@ const init = async () => {
 
     // if user exits
     if (dbData.hasOwnProperty(username) && dbData[username].hasOwnProperty(repoName)) {
+        console.log("INFO : \n");
+        // current info
+        // console.log(`${stargazers.length} people have starred this repo!`);
+
+        // // people that unstared
+        // let unstargazers = getUnstargazers(dbData, stargazers);
+        // console.log(`${unstargazers} people have unstarred this repo since ${dbData[username][repoName]["time"]}`);
+
+        printLongArray(dbData[username][repoName]["stargazers"], 100);
+
+        // new people that have starred
 
 
     } else {
         // if user's repo exists
         console.log("user doesn't exist");
-        addNewData(dbData, username, repoName, stargazers)
+        // addNewData(dbData, username, repoName, stargazers)
         console.log("New data added!!");
     }
 }
