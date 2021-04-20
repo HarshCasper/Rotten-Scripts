2c1032a70f6a84e2bcb5fadc6b67cd1119651228
jhamadhav <jhamadhav28@gmail.com>
2021-04-20 12:28:04 +0530

    pseudo code

--- JavaScript/Twitter_Followers_Overlap/twitter_followers_overlap.js
+++ JavaScript/Twitter_Followers_Overlap/twitter_followers_overlap.js
@@ -9,4 +9,20 @@ const init = () => {
      * 3. compare them and get the overlap
      * 4. output the overlap
      */
+
+    let user1 = prompt("Enter first username : ");
+    let user2 = prompt("Enter second username : ");
+
+    let user1Followers = twt.getFollowers();
+    let user2Followers = twt.getFollowers();
+
+    let overlapFollowers = getOverlappingFollowers();
+
+    console.log("===============================");
+    console.log("---Twitter Followers Overlap---");
+    console.log("===============================");
+
+    console.log("\n\nOverlapping followers are : \n");
+    printLongArray(overlapFollowers);
+
 }
