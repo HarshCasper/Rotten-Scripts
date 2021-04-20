9628b0c838136b034ae4f5d32def83ec095353a3
jhamadhav <jhamadhav28@gmail.com>
2021-04-20 22:26:00 +0530

    pseudo code added

--- JavaScript/Github_Good_First_Issues/Github_Good_First_Issues.js
+++ JavaScript/Github_Good_First_Issues/Github_Good_First_Issues.js
@@ -1,1 +1,36 @@
-console.log("hello");
+const prompt = require("prompt-sync")()
+
+const init = () => {
+    /**
+     * 1. ask user from three options
+     * a. Having a JSON File where all the repositories are stored.
+     * b. Scrapping from the top repositories that are trending on GitHub Explore
+     * c. Scrapping from the repos that we/or someone else has starred
+     * 
+     * 2. fetch repos
+     * 3. fetch issues with "Good first issues" label
+     * 4. display them
+    */
+    console.log("\n================================");
+    console.log("----Github Good First Issues----");
+    console.log("================================\n");
+
+
+    console.log("1. From the JSON file in the folder (enter 1)")
+    console.log("2. From the trending page of Github (enter 2)")
+    console.log("3. From the starred repos of an user (enter 3)")
+    let method = prompt("Choose method to fetch repos : ")
+
+    let username = null;
+    if (Number(method) == 3) {
+        username = prompt("\nEnter github username : ")
+    }
+
+    let repos = getRepos(method, username)
+    let issues = getIssues(repos)
+
+    printIssues(issues);
+}
+
+// entry function
+init();
