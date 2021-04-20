c5287c23754a6b74dde1cda185de5ba12ccb7d10
jhamadhav <jhamadhav28@gmail.com>
2021-04-20 12:23:32 +0530

    twitter functions added

--- JavaScript/Twitter_Followers_Overlap/twitter_followers_overlap.js
+++ JavaScript/Twitter_Followers_Overlap/twitter_followers_overlap.js
@@ -1,1 +1,12 @@
-console.log("hi");
+const prompt = require("prompt-sync")();
+const api = require("./apiTokens")
+const twt = require("./twitterFunc")
+
+const init = () => {
+    /**
+     * 1. ask names of two user
+     * 2. get their followers
+     * 3. compare them and get the overlap
+     * 4. output the overlap
+     */
+}
