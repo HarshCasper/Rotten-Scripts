2c18dc7c6a5785d1783281cfce2568ade6c35c0d
rohit <rohitsdec4@gmail.com>
2021-04-21 09:59:59 +0530

    Added method to filter files

--- Python/Bulk_File_Renamer/utils.py
+++ Python/Bulk_File_Renamer/utils.py
@@ -1,14 +1,5 @@
-from os.path import isdir, isfile
+from os.path import abspath, isdir, isfile, getsize
 from os import listdir
-
-def list_all_files(path):
-    """Returns a List object of all the files present at the path argument"""
-
-    if not isdir(path):
-        raise NotADirectoryError("Invalid value passed to path argument")
-    
-    files = [file for file in listdir(path) if isfile(file)]
-    return files
 
 class SelectFiles:
 
@@ -43,16 +34,57 @@ class SelectFiles:
         for key in self.FILTERS:
             while True:
                 value = self.validate(input(self.FILTERS[key]['description']), key)
-                if value != False:
+                if value == "":
+                    break
+                elif value != False:
                     self.FILTERS[key]['apply'] = True
                     self.FILTERS[key]['value'] = value
                     break
             
-        print(self.FILTERS)
-        
-    def filter(self):
-        pass
+    @staticmethod
+    def list_all_files(path):
+        """Returns a List object of all the files present at the path argument"""
 
+        if not isdir(path):
+            raise NotADirectoryError("Invalid value passed to path argument")
+        
+        path = abspath(path)
+
+        return ["{}/{}".format(path, file) for file in listdir(path) if isfile(f"{path}/{file}")]
+
+
+    @staticmethod
+    def apply_filter(files, filter_argument, filter_value):
+        """Takes in a filter_argument and a filter_value, based on which it filters\
+            out the list of files"""
+
+        if filter_argument == "filesize":
+            # Convert filesize from Kb to Bytes
+            size = filter_value*1024
+            
+            return [file for file in files if getsize(file) <= size]
+
+
+    def filter(self, path=None):
+        """Looks for all the files located at 'path' argument, and applies the filters to them"""
+        if path is None:
+            raise ValueError("Invalid value passed to path argument")
+        
+        files = SelectFiles.list_all_files(path)
+
+        print(f"\nNo of files at '{abspath(path)}': {len(files)}")
+
+        if len(files) == 0:
+            return []
+        
+        for key in self.FILTERS:
+            if self.FILTERS[key]["apply"]:
+                files = SelectFiles.apply_filter(files, key, self.FILTERS[key]["value"])
+        
+        print("Filtered files: ")
+        print(*files, sep="\n")
+
+    
     def validate(self, value, key):
         """Returns the value as it is, if it is valid or raises a ValueError"""
 
@@ -60,11 +92,19 @@ class SelectFiles:
             if value != "" and int(value) <= 0:
                 print("Error: Filesize must be greater than or equal to 0")
                 return False
-        elif key == "time_of_modification":
-            # TODO
-            pass
+            else:
+                return int(value)
 
-        return 
+        elif key == "extension":
+            if value != "":
+                return value.split(" ")
+
+        elif key == "time_of_modification":
+            if value != "" and value:
+                return False
+            # TODO
+
+        return value
 
 def select_files(files):
     """Selects files from the passed object based on different criterion"""
