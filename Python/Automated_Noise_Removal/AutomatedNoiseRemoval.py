f45b8773e990df3e92a396789e06971278851d3d
Rahul Rana <me.rahulrana@outlook.com>
2021-04-22 18:25:03 +0530

    updated

--- Python/Automated_Noise_Removal/AutomatedNoiseRemoval.py
+++ Python/Automated_Noise_Removal/AutomatedNoiseRemoval.py
@@ -3,8 +3,8 @@
 OPEN-SOURCE SCRIPT FOR AUTOMATED NOISE REMOVAL USING PYTHON.
 
 SOFTWARE/LIBRARY REQUIRED TO RUN THIS SCRIPT:
-    1. SoX - ( http://sox.sourceforge.net/ )
-    2. pydub - ( https://pypi.org/project/pydub/ )
+	1. SoX - ( http://sox.sourceforge.net/ )
+	2. pydub - ( https://pypi.org/project/pydub/ )
 
 CREATED AND SUBMITTED BY RAHUL RANA @owlrana during GSSoC'21 PERIOD for https://github.com/HarshCasper/Rotten-Scripts
 
