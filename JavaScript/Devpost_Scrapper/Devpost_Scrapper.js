b379d236570b7d83c1e0f3f6a793cccbfb14162c
jhamadhav <jhamadhav28@gmail.com>
2021-04-22 12:50:05 +0530

    pseudo code

--- JavaScript/Devpost_Scrapper/Devpost_Scrapper.js
+++ JavaScript/Devpost_Scrapper/Devpost_Scrapper.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const prompt = require("prompt-sync")()
+const scrapper = require("./scrapper")
+const init = () => {
+    /*
+     * 1.get hackathons link as input
+     * 2.get hackathons html page data
+     * 3.extract project links from page
+     * 4.get project info
+     * 5.display project data 
+     */
+    let hackathonsLink = prompt("Enter DevPost link : ")
+    let hackathonsData = scrapper.getHackathonsData(hackathonsLink)
+
+    let hackathonsProjectsURL = `${hackathonsLink} + /project-gallery`
+    let projectLinks = scrapper.getProjects(hackathonsProjectsURL)
+    let projectData = scrapper.getProjectsData(hackathonsProjectsURL)
+
+    displayProjectInfo(projectData)
+
+}
+
+// entry function
+init()
