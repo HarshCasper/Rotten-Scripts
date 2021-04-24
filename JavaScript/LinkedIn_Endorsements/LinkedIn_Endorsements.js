18d7708164a6d0bb9cf8aa53f9081b9a599bb413
jhamadhav <jhamadhav28@gmail.com>
2021-04-24 09:38:31 +0530

    pseudo functions written

--- JavaScript/LinkedIn_Endorsements/LinkedIn_Endorsements.js
+++ JavaScript/LinkedIn_Endorsements/LinkedIn_Endorsements.js
@@ -1,3 +1,10 @@
+const prompt = require("prompt-sync")()
+const scrapper = require("./scrapper")
+const chalk = require("chalk")
+
+const displayEndorsements = (data) => {
+
+}
 
 const init = async () => {
     /**
@@ -5,6 +12,16 @@ const init = async () => {
      * 2. scrape endorsements
      * 3. display them
      */
+    console.log("\n===================================");
+    console.log("---LinkedIn Endorsement scrapper---");
+    console.log("===================================\n");
+
+    let profileLink = prompt("Enter LinkedIn profile link : ")
+
+    let endorsements = await scrapper.getEndorsements(profileLink)
+    displayEndorsements()
+
+    console.log("\n---END---\n")
 }
 
 // entry function
