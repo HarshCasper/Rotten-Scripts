526889e8b134453873948383cf26f4b3a5dfb9ef
jhamadhav <jhamadhav28@gmail.com>
2021-04-24 09:26:58 +0530

    pseudo code written

--- JavaScript/LinkedIn_Endorsements/LinkedIn_Endorsements.js
+++ JavaScript/LinkedIn_Endorsements/LinkedIn_Endorsements.js
@@ -1,1 +1,11 @@
-console.log("hello");
+
+const init = async () => {
+    /**
+     * 1. get LinkedIn profile link
+     * 2. scrape endorsements
+     * 3. display them
+     */
+}
+
+// entry function
+init()
