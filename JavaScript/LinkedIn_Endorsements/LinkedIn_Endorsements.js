a54a56a663ec0d5fa82037cae6417b8d616cc1d7
jhamadhav <jhamadhav28@gmail.com>
2021-05-13 01:01:40 +0530

    comments removed

--- JavaScript/LinkedIn_Endorsements/LinkedIn_Endorsements.js
+++ JavaScript/LinkedIn_Endorsements/LinkedIn_Endorsements.js
@@ -26,11 +26,6 @@ const displayEndorsements = (data) => {
 }
 
 const init = async () => {
-    /**
-     * 1. get LinkedIn profile link
-     * 2. scrape endorsements
-     * 3. display them
-     */
     console.log("\n===================================");
     console.log("---LinkedIn Endorsement scrapper---");
     console.log("===================================\n");
