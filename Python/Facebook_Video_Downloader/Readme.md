fd3b61e31d1550d6994b20641167eb6e65619cdd
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2021-06-01 00:29:47 +0530

    added facebook video downloader script (#1089)
    * added facebook video downloader script
    
    * Update Readme.md

---
+++ Python/Facebook_Video_Downloader/Readme.md
@@ -0,0 +1,27 @@
+# Facebook Video Downloader
+
+From your terminal download any facebook video
+
+## Installation
+
+> pip3 install -r requirements.txt
+
+
+## Usage
+- Run command python3 script.py
+- You will asked for URL
+- Provide the URL and then enter , your video will get downloaded
+
+## Output
+
+Downloaded video you desired in the same directory of the script.
+
+## Demo
+
+![ezgif com-gif-maker (4)](https://user-images.githubusercontent.com/55010599/120222549-bd621180-c25d-11eb-82ab-8177a8461e57.gif)
+
+
+## Authors
+
+Written by [Sukriti-sood](https://www.github.com/Sukriti-sood).
+
---
+++ Python/Facebook_Video_Downloader/script.py
@@ -0,0 +1,55 @@
+# ALL Imports
+from tqdm import tqdm
+from requests import get, HTTPError, ConnectionError
+from re import findall
+from urllib.parse import unquote
+
+def Invalid_Url():
+    print("Invalid URL , Please Enter Correct URL")
+
+def get_video_downloadlink(url):
+    
+    url = url.replace("www", "mbasic")
+    try:
+        r = get(url, timeout=5, allow_redirects=True)
+        if r.status_code != 200:
+            raise HTTPError
+        a = findall("/video_redirect/", r.text)
+        if len(a) == 0:
+            print("[!] Video Not Found...")
+            exit(0)
+        else:
+            return unquote(r.text.split("?src=")[1].split('"')[0])
+    except (HTTPError, ConnectionError):
+        print("[x] Invalid URL")
+        exit(1)
+
+def download_video(url):
+
+    block_size = 1024  # 1kB
+    r = get(url, stream=True)
+    total_size = int(r.headers.get("content-length"))
+    progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
+    with open('video.mp4', 'wb') as file:
+        for data in r.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    print("Video Downloaded")
+    if total_size != 0 and progress_bar.n != total_size:
+        print("ERROR, something went wrong")
+
+
+def main():
+    url=input("Enter the URL of Facebook Video you want to download:- ")
+    if not "www.facebook.com" in url:
+        Invalid_Url()
+        return
+
+    link=get_video_downloadlink(url)
+    download_video(link)
+
+
+
+if __name__ == "__main__":
+    main() 
