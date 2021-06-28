ca0d5148c1e2c9d418f0ed85b74e051586941031
Damarla Venkata Sai Chandana <66585358+chandu6111@users.noreply.github.com>
2021-06-28 16:53:48 +0530

    Update pdf_reader.py

--- Python/PDF_Reader/pdf_reader.py
+++ Python/PDF_Reader/pdf_reader.py
@@ -2,49 +2,53 @@
 import pyttsx3
 import PyPDF2
 
-# initializing the speaker
-speaker = pyttsx3.init()
+def main():
+    # initializing the speaker
+    speaker = pyttsx3.init()
 
-# enter the file path with the pdf name
-path = input("Enter the file path of the pdf along with pdf name: ")
+    # enter the file path with the pdf name
+    path = input("Enter the file path of the pdf along with pdf name: ")
 
-# opening the document in read mode. You can mention your document name here
-book = open(path, 'rb')
+    # opening the document in read mode. You can mention your document name here
+    book = open(path, 'rb')
 
-# creating an object of PyPDF2 and calling the PdfFileReader method
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
+    # creating an object of PyPDF2 and calling the PdfFileReader method
+    pdfReader = PyPDF2.PdfFileReader(book)
+    pages = pdfReader.numPages
 
-# telling the number of pages in the document
-print("Number of pages in the document are: "+str(pages))
-speaker.say("Number of pages in the document are ")
-speaker.runAndWait()
-speaker.say(pages)
-speaker.runAndWait()
-
-# asking which page has to be read
-speaker.say("Which page do you want me to read?")
-speaker.runAndWait()
-print("Which page do you want me to read?")
-# input the start page number
-start_page_number = int(input())
-
-# asking the end page number
-speaker.say("Till what page number should I read?")
-speaker.runAndWait()
-print("Till what page number should I read?")
-# input the end page number till which document has to be read
-last_page_number = int(input())
-
-for i in range(start_page_number-1, last_page_number):
-    # get page number
-    page = pdfReader.getPage(i)
-    # extracting text from that page
-    text = page.extractText()
-    # speaker starts reading the text in pdf
-    speaker.say(text)
+    # telling the number of pages in the document
+    print("Number of pages in the document are: "+str(pages))
+    speaker.say("Number of pages in the document are ")
     speaker.runAndWait()
-speaker.say('Hurray, done with reading !!')
-speaker.runAndWait()
-print('Hurray, done with reading !!')
+    speaker.say(pages)
+    speaker.runAndWait()
+
+    # asking which page has to be read
+    speaker.say("Which page do you want me to read?")
+    speaker.runAndWait()
+    print("Which page do you want me to read?")
+    # input the start page number
+    start_page_number = int(input())
+
+    # asking the end page number
+    speaker.say("Till what page number should I read?")
+    speaker.runAndWait()
+    print("Till what page number should I read?")
+    # input the end page number till which document has to be read
+    last_page_number = int(input())
+
+    for i in range(start_page_number-1, last_page_number):
+        # get page number
+        page = pdfReader.getPage(i)
+        # extracting text from that page
+        text = page.extractText()
+        # speaker starts reading the text in pdf
+        speaker.say(text)
+        speaker.runAndWait()
+    speaker.say('Hurray, done with reading !!')
+    speaker.runAndWait()
+    print('Hurray, done with reading !!')
+
+if __name__ == '__main__':
+    main()
 
