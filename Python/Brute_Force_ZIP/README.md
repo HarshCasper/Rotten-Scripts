f412167a7d3c80dac08f0b0ecceab95e1bb4e264
cyb3r-g0d <btharun.2003@gmail.com>
2021-07-02 03:55:35 +0530

    Brute_Force_ZIP

---
+++ Python/Brute_Force_ZIP/README.md
@@ -0,0 +1,29 @@
+# Brute force zip file
+
+## Description
+
+An simple python tool to crack a password protected zip file by dictionary attack
+
+## Requirements
+
+You need tqdm module to run this python script
+
+```sh
+pip install tqdm
+```
+
+## Usage
+
+- Run the python script
+
+```sh
+python zip.py
+```
+
+- Enter the directory of the zip file you want to crack. 
+
+## Output
+
+[Result](https://i.ibb.co/tCg10rj/output.png)
+
+### A script by [CYB3R G0D](https://github.com/cyb3r-g0d)
---
+++ Python/Brute_Force_ZIP/zip.py
@@ -0,0 +1,25 @@
+# By CYB3R G0D
+
+# pip3 install tqdm
+from tqdm import tqdm
+import zipfile
+
+
+# the wordlist path (in current dir)
+wordlist = 'rockyou.txt'
+# the zip file path
+file = input('zip file directory: ')
+file = zipfile.ZipFile(file)
+n_words = len(list(open(wordlist, 'rb')))
+# print total number of passwords in the list
+print('Total passwords to test:', n_words)
+with open(wordlist, 'rb') as wordlist:
+    for word in tqdm(wordlist, total=n_words, unit='word'):
+        try:
+            file.extractall(pwd=word.strip())
+        except:
+            continue
+        else:
+            print('[+] Password found:', word.decode().strip())
+            exit(0)
+print('[!] Password not found in', wordlist)
