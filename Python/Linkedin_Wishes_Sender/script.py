48ea277e7ea9d193555901f24691885c4eae0f74
Sukriti-sood <soodsukriti1111@gmail.com>
2021-07-05 08:12:45 +0530

    completed

--- Python/Linkedin_Wishes_Sender/script.py
+++ Python/Linkedin_Wishes_Sender/script.py
@@ -6,17 +6,15 @@ from pathlib import Path
 import os
 import time
 
-env_path = Path('.', '.env')
+env_path = Path(".", ".env")
 load_dotenv(dotenv_path=env_path)
+
 
 def read_creds():
 
-    """This function reads the environmental variables."""    
+    """This function reads the environmental variables."""
 
-    credentials={
-        'username':os.getenv("username"),
-        'password':os.getenv('password')
-    }
+    credentials = {"username": os.getenv("username"), "password": os.getenv("password")}
     return credentials
 
 
@@ -25,90 +23,101 @@ def login(browser):
     """This function help in Login to Linkedin.
     Arguments:
     browser: browser where task runs
-    """     
+    """
 
-    linkedin_url="https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
-    creds=read_creds()
+    linkedin_url = "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
+    creds = read_creds()
     browser.get(linkedin_url)
-    elementID=browser.find_element_by_id("username")
-    elementID.send_keys(creds['username'])
-    elementID=browser.find_element_by_id("password")
-    elementID.send_keys(creds['password'])
+    elementID = browser.find_element_by_id("username")
+    elementID.send_keys(creds["username"])
+    elementID = browser.find_element_by_id("password")
+    elementID.send_keys(creds["password"])
     elementID.submit()
+
 
 def count_notification(browser):
     """This function help in returning the notification count.
     Arguments:
     browser: browser where task runs
-    """   
-    pagesource=browser.page_source;
-    soup=BeautifulSoup(pagesource,'html.parser')
-    if soup.find("span",class_="notification-badge__count")!=None :
-        element=browser.find_element_by_css_selector("span.notification-badge__count");
-        return int(element.text);
+    """
+    pagesource = browser.page_source
+    soup = BeautifulSoup(pagesource, "html.parser")
+    if soup.select_one("a[data-test-global-nav-link='notifications'] span.notification-badge__count") != None:
+        element = browser.find_element_by_css_selector(
+            "a[data-test-global-nav-link='notifications'] span.notification-badge__count"
+        )
+        return int(element.text)
     else:
-        return 0;
+        return 0
 
 
 def wish(browser):
     """This function help in sending wish.
     Arguments:
     browser: browser where task runs
-    """   
+    """
     login(browser)
     time.sleep(2)
-    notification_count=count_notification(browser)
+    notification_count = count_notification(browser)
+    print(notification_count)
 
     # It check whether there are any new notification or not
-    if notification_count==0:
-        #If no new notification then script will not run
-        print("Currently there are no new Notifications");
+    if notification_count == 0:
+        # If no new notification then script will not run
+        print("Currently there are no new Notifications")
         browser.close()
     else:
-        element =browser.find_element_by_css_selector('a[data-test-global-nav-link="notifications"]');
-        element.click();
-        time.sleep(5);
+        element = browser.find_element_by_css_selector('a[data-test-global-nav-link="notifications"]')
+        element.click()
+        time.sleep(5)
         # All notification cards
-        msg_sent=0
-        elements=browser.find_elements_by_css_selector(".nt-segment__occludable-area.ember-view")
-        count=0
+        msg_sent = 0
+        elements = browser.find_elements_by_css_selector(".nt-segment__occludable-area.ember-view")
+        count = 0
         for element in elements:
-            if count>=notification_count:
+            if count >= notification_count:
                 break
-            count+=1
-            element_source=element.get_attribute('innerHTML')
-            soup=BeautifulSoup(element_source,'html.parser')
-            
+            count += 1
+            element_source = element.get_attribute("innerHTML")
+            soup = BeautifulSoup(element_source, "html.parser")
+
             # Check for button that directs to message box
-            if soup.select_one(".message-anywhere-button.artdeco-button.artdeco-button--secondary.artdeco-button--default")!=None:     
-                wish_btn=element.find_element_by_css_selector(".message-anywhere-button.artdeco-button.artdeco-button--secondary.artdeco-button--default")
-                
+            if (
+                soup.select_one(
+                    ".message-anywhere-button.artdeco-button.artdeco-button--secondary.artdeco-button--default"
+                )
+                != None
+            ):
+                wish_btn = element.find_element_by_css_selector(
+                    ".message-anywhere-button.artdeco-button.artdeco-button--secondary.artdeco-button--default"
+                )
+
                 # If message is regaring birthday
                 if "happy birthday" in element.text:
-                    wish_text=element.find_element_by_css_selector(".nt-card__text--3-line span.visually-hidden").text
-                    date_txt=wish_text.split("(")[1].split(")")[0];
+                    wish_text = element.find_element_by_css_selector(".nt-card__text--3-line span.visually-hidden").text
+                    date_txt = wish_text.split("(")[1].split(")")[0]
 
                     # If the day of birthday is not same as of reading notfication
-                    if date_txt!="today":
+                    if date_txt != "today":
                         # then script will not send message
                         continue
                 wish_btn.click()
                 time.sleep(2)
-                msg_box=browser.find_element_by_css_selector(".msg-overlay-conversation-bubble")
-                send_btn=msg_box.find_element_by_css_selector(".msg-form__send-button")
+                msg_box = browser.find_element_by_css_selector(".msg-overlay-conversation-bubble")
+                send_btn = msg_box.find_element_by_css_selector(".msg-form__send-button")
                 send_btn.click()
                 time.sleep(1)
-                clos_btn=msg_box.find_element_by_css_selector("button[data-control-name='overlay.close_conversation_window']")
+                clos_btn = msg_box.find_element_by_css_selector(
+                    "button[data-control-name='overlay.close_conversation_window']"
+                )
                 clos_btn.click()
-                msg_sent+=1
+                msg_sent += 1
                 print("Send greetings")
 
-        if msg_sent==0:
+        if msg_sent == 0:
             print("Currently No new Notification for any wish")
-            
 
-        browser.close()        
-
+        browser.close()
 
 
 def main():
@@ -117,7 +126,5 @@ def main():
     wish(browser)
 
 
-
 if __name__ == "__main__":
     main()
-    
