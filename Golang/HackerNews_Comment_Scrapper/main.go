0499d46e5dcab929d1dfa61ed8db0299b45a5938
Sukriti-sood <soodsukriti1111@gmail.com>
2021-08-11 16:10:19 +0530

    made script dynamic

--- Golang/HackerNews_Comment_Scrapper/main.go
+++ Golang/HackerNews_Comment_Scrapper/main.go
@@ -3,48 +3,55 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
 	"io/ioutil"
-    "log"
+	"log"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
-type Comment struct{
-	Comment_text string 
+type Comment struct {
+	Comment_text string
 }
-func main(){
-	comments := make([]Comment,0)
+
+func main() {
+	fmt.Print("Enter the URL of HackerNews Thread:- ")
+
+	var url string
+
+	fmt.Scan(&url)
+	comments := make([]Comment, 0)
 
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	c.OnHTML(".comment-tree .comment", func(element *colly.HTMLElement){
-	comm:=strings.TrimSpace(element.Text)
+	c.OnHTML(".comment-tree .comment", func(element *colly.HTMLElement) {
+		comm := strings.TrimSpace(element.Text)
 
-	comment:= Comment{
-		Comment_text: comm,
-	}
+		comment := Comment{
+			Comment_text: comm,
+		}
 
-	comments = append(comments,comment)
-})
+		comments = append(comments, comment)
+	})
 
-c.OnRequest(func(request *colly.Request) {
-	fmt.Println("Visiting", request.URL.String())
-})
+	c.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
 
-c.Visit("https://news.ycombinator.com/item?id=27750040") // Change URL here
+	c.Visit(url)
 
-writeJSON(comments)
+	writeJSON(comments)
 
 }
 
 func writeJSON(data []Comment) {
-    file, err := json.MarshalIndent(data, "", " ")
-    if err != nil {
-        log.Println("Unable to create json file")
-        return
-    }
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Println("Unable to create json file")
+		return
+	}
 
-    _ = ioutil.WriteFile("output.json", file, 0644)
+	_ = ioutil.WriteFile("output.json", file, 0644)
 	fmt.Println("output.json Added")
 }
