2773cb8e58a4cea5392490f0b75a25871bc1cd61
Amankesarwani2202 <64828784+Amankesarwani2202@users.noreply.github.com>
2021-08-18 00:45:08 +0530

    Update image_resizer.py

--- Python/Image_Resizer/image_resizer.py
+++ Python/Image_Resizer/image_resizer.py
@@ -1,1 +1,12 @@
 
+base_width = 360
+
+image = Image.open('example-image.jpg')
+
+width_percent = (base_width / float(image.size[0]))
+
+hsize = int((float(image.size[1]) * float(width_percent)))
+
+image = image.resize((base_width, hsize), PIL.Image.ANTIALIAS)
+
+image.save('resized_compressed_image.jpg') 
