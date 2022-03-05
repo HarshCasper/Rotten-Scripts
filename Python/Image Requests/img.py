718d34825b1e35165749bacba481f827bb39531b
Alapan Das <71692310+AlapanDas@users.noreply.github.com>
2022-03-05 20:04:27 +0530

    Update img.py

--- Python/Image Requests/img.py
+++ Python/Image Requests/img.py
@@ -1,15 +1,17 @@
 import requests
 from PIL import Image
-def img_requests(txt):
+
+def img_requests(txt):#this function will help the driver code to provide images with keywords.
     response=requests.get("https://source.unsplash.com/random/{0}".format(txt))
-    file=open('sample_image.jpg','wb')
+    file=open('container.jpg','wb')
     file.write(response.content)
     img=Image.open(r"container.jpg")
     img.show()
     file.close
-def img_random(txt):
+
+def img_random(txt):#this function will help the driver code to provide random images.
     response=requests.get("https://source.unsplash.com/random?{0}".format(txt))
-    file=open('sample_image.jpg','wb')
+    file=open('container.jpg','wb')
     file.write(response.content)
     img=Image.open(r"container.jpg")
     img.show()
@@ -21,21 +23,21 @@ choice=int(input("""Please provide an option for Image
 3.2K Random Picture
 4.4k Random Picture
 5.Picture with User Keywords """))
+
+print("Please wait while we fetch the images from our database.")
+
+#checking the user's choice.
+
 if choice==1:
     img_requests('1280x720')
-    print("Please wait while we fetch the images from our database.")
 elif choice==2:
     img_requests('1920x1080')
-    print("Please wait while we fetch the images from our database.")
 elif choice==3:
     img_requests('2048x1080')
-    print("Please wait while we fetch the images from our database.")
 elif choice==4:
     img_requests('4096x2160')
-    print("Please wait while we fetch the images from our database.")
 elif choice==5:
     st=input("Enter keywords seperated by commas ")
-    print("Please wait while we fetch the images from our database.")
     img_random(st)
 else:
     print("Please provide a valid Input")
