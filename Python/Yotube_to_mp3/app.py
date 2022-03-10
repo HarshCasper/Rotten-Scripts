63cc8d8bd2ecff848b415c7258c5c54fed108be2
Debayan <55997552+Phoenix-031@users.noreply.github.com>
2022-03-11 01:55:48 +0530

    update app.py

--- Python/Yotube_to_mp3/app.py
+++ Python/Yotube_to_mp3/app.py
@@ -1,66 +1,55 @@
 import tkinter as tk
-from tkinter import StringVar,filedialog
+from tkinter import StringVar,filedialog, messagebox
 from subprocess import run
 import string
 
 
 floralwhite = '#FFFAF0'                   ## bacground color off app
 
-class downloader():
+def submit(link,path):
+    
+    if path != None:
+        run(f'youtube-dl --prefer-ffmpeg -o "{path}/%(title)s.%(ext)s" --extract-audio --audio-format mp3 {link}',
+             shell=True, capture_output=True, text=True).stdout
+    else:
+         messagebox.showinfo("Downloader", "no link is specified")
+            
+    url_entry.delete(0,tk.END)
+    save_entry.delete(0,tk.END)
 
-    def __init__(self):
 
+def browse(self):
+   filename = filedialog.askdirectory()
+   f_path.set(filename)
+    
 ## creates the main window
 
-        self.win = tk.Tk()
-        self.win.geometry("600x150")
-        self.win.title("Youtube To Mp3")
+win = tk.Tk()
+win.geometry("600x150")
+win.title("Youtube To Mp3")
 
 ## creates the different elements in the window
 
-        self.url = tk.Label(self.win,text= "Video Link",bg=floralwhite,fg='#000000',font= ('Arial',12))
-        self.url.grid(row=0,column=0,padx=10,pady=20)
-        self.url_entry = tk.Entry(self.win,bg=floralwhite,border=2,width=60)
-        self.url_entry.grid(row=0,column=1,padx=5,pady=20)
-        self.url_entry.focus_set()
+url = tk.Label(win,text= "Video Link",bg=floralwhite,fg='#000000',font= ('Arial',12))
+url.grid(row=0,column=0,padx=10,pady=20)
+url_entry = tk.Entry(win,bg=floralwhite,border=2,width=60)
+url_entry.grid(row=0,column=1,padx=5,pady=20)
+url_entry.focus_set()
 
-        self.f_path = StringVar()
-        self.savelbl = tk.Label(self.win,bg=floralwhite,font = ('Arial',12),fg = '#000000',text = "Save mp3 file")
-        self.savelbl.grid(row=1,column=0,padx=5,pady=5)
-        self.save_entry = tk.Entry(self.win,bg=floralwhite,border=2,width=60,textvariable=self.f_path)
-        self.save_entry.grid(row=1,column=1,padx=10)
+f_path = StringVar()
+savelbl = tk.Label(win,bg=floralwhite,font = ('Arial',12),fg = '#000000',text = "Save mp3 file")
+savelbl.grid(row=1,column=0,padx=5,pady=5)
+save_entry = tk.Entry(win,bg=floralwhite,border=2,width=60,textvariable=f_path)
+save_entry.grid(row=1,column=1,padx=10)
 
 ## creates the select foleder button
-        self.browse_btn = tk.Button(self.win,bg=floralwhite,border=2,text = "Browse",fg="#000000",command=self.browse)
-        self.browse_btn.grid(row=1,column=2,padx=5,pady=5)
+browse_btn = tk.Button(win,bg=floralwhite,border=2,text = "Browse",fg="#000000",command=browse)
+browse_btn.grid(row=1,column=2,padx=5,pady=5)
 
 ## creates the download button
-        self.submit_btn = tk.Button(self.win,text = "Submit", bg=floralwhite,fg='#000000',border=2,command= lambda : self.submit(self.url_entry.get(),self.save_entry.get()))
-        self.submit_btn.grid(row=2,column=1,padx=15,pady=10)
+submit_btn = tk.Button(win,text = "Submit", bg=floralwhite,fg='#000000',border=2,command= lambda : self.submit(url_entry.get(),save_entry.get()))
+submit_btn.grid(row=2,column=1,padx=15,pady=10)
 
-        self.win.configure(bg=floralwhite)
-        self.win.mainloop()
+win.configure(bg=floralwhite)
+win.mainloop()
 
-
-    def submit(self,link,path):
-        self.url_entry.delete(0,tk.END)
-        self.save_entry.delete(0,tk.END)
-        self.link = link
-        self.path = path
-
-        if self.path != None:
-            run(f'youtube-dl --prefer-ffmpeg -o "{self.path}/%(title)s.%(ext)s" --extract-audio --audio-format mp3 {self.link}',
-                 shell=True, capture_output=True, text=True).stdout
-        else:
-             run(f'youtube-dl --prefer-ffmpeg --extract-audio --audio-format mp3 {self.link}',
-                 shell=True, capture_output=True, text=True).stdout
-
-
-    def browse(self):
-       self.filename = filedialog.askdirectory()
-       self.f_path.set(self.filename)
-    
-
-
-if __name__ == '__main__':
-    downloader()
