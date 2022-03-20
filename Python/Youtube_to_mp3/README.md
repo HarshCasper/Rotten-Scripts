c8f43666bb837c0523ee4a3f31a26804dc4b170e
Debayan <pradhandebayan@gmail.com>
2022-03-20 09:38:24 +0530

    added youtube to mp3 converter script

---
+++ Python/Youtube_to_mp3/README.md
@@ -0,0 +1,11 @@
+## Youtube to mp3 converter
+
+A youtube video to mp3 converter will take a youtube link in and a directory to save, download and convert the youtube video to mp3 and save it in the mentioned directory or in the defult directory
+
+## Usage
+
+- cd /Rotten-Scripts/Python/Youtube_to_mp3
+- pip install -r requirements.txt
+- run the app.py file
+- put the link and select the directory
+- download and enjoy..
---
+++ Python/Youtube_to_mp3/script.py
@@ -0,0 +1,34 @@
+import  youtube_dl
+import os
+
+def download():                                                                        # main download function to download and convert
+    video_url = input("Enter youtube url: ")                                           # enter he video url
+    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
+    base = os.path.expanduser('~')                                                      # gets the default user path 
+    redo =True
+    while redo:
+
+        file_dir = input("enter path to save the downloaded file(q to save in default location): ")
+        if file_dir == 'q':
+            file_dir = base + "\\Desktop"
+
+        elif os.path.exists(file_dir):                                                   # validates weather input path is valid or not
+            redo = False 
+
+    file_name = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':file_dir + "\\" + file_name,
+    }
+ 
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+    print("download completed!!!.")
+
+
+if __name__ == '__main__':                                                            # main function
+    download()
+	
+	
+	
