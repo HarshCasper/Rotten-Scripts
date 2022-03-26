7bd5c0cbc9eb31a2ded93c4663ebd826bb754c77
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-03-27 02:34:51 +0530

    Delete index.js

--- JavaScript/GetCovid19Statistics/index.js
+++
@@ -1,7 +0,0 @@
-const covid19 = require('covid19-stats')
-const main = async (inputs, auths, context) => {
-  const country = inputs.country
-  var stats = await covid19.getCountry(country)
-  return stats
-}
-module.exports.main = main
