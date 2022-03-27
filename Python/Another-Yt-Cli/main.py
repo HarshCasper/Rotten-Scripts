4ae29709e4cc5f31f36b8b5d4b4e9b90cfa68a09
Skyhero-admin <smnouman007@gmail.com>
2022-03-27 14:34:02 +0530

    Refactor: Removed unused import and come commented code

--- Python/Another-Yt-Cli/main.py
+++ Python/Another-Yt-Cli/main.py
@@ -1,29 +1,17 @@
 import requests, re, json, os, sys
-from lxml import etree
 from bs4 import BeautifulSoup
 
 def get_link(name):
 	query = name.split()
 	r = requests.get('https://www.youtube.com/results?search_query='+ "+".join(query)).text
-	# html = r.content
-	# print('https://www.youtube.com/results?search_query='+ "+".join(name))
 	print("Searching for "+name+"....\n")
 
 	soup = BeautifulSoup(r, "lxml")
 
 	script = soup.find_all("script")[33]
-	# print(script)
 
 	json_text = re.search("var ytInitialData = (.+)[,;]{1}", str(script)).group(1)
 	json_data = json.loads(json_text)
-	# print(json_data)
-
-	# ME TESTING :)
-	# with open("file.txt", "w", encoding='utf-8') as output:
-	# 	output.write(soup.prettify())
-
-	# with open("script.txt", "w", encoding = "utf-8") as output:
-	# 	output.write(json_text)
 
 	content = (
 		json_data
@@ -41,23 +29,10 @@ def get_link(name):
 					# print(v)
 					try:
 						if k == "videoId" :
-							# and len(v) == 11
-							# print(v)
 							return "https://www.youtube.com/watch?v="+v
-						# if k =="title":
-							# print(v["runs"][0]["text"])
 					except Exception as e:
 						print("An error occured:",e)
 
-	# dom = etree.HTML(str(soup))
-	# print(dom.xpath('//*[@id="video-title"]')[0].text)
-	# for i in res:
-	# 	print(i)
-		# print(i.a['id'])
-		# if i.a['id'] == "video-title":
-		# 	print(i)
-
-# get_link()
 
 def get_video_name():
 	name = input("Enter the video name you want to play: ")
