0f2e9f4f78c9fd7e9e2a4f8e8203ba82cbf8fa94
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-30 20:48:02 -0700

    Update script.py

--- Python/audiobook/script.py
+++ Python/audiobook/script.py
@@ -9,8 +9,7 @@ ab = AudioBook(speed="normal") # default speed is normal/slow/fast
 
 def read_book(input_file_path):
     """ if you want to read your book """
-    ab.create_json_book(input_file_path)
-
+    ab.read_book(input_file_path)
 
 def save_audio(input_file_path):
     """ if you want to save your audio books """
@@ -25,4 +24,4 @@ if __name__ == "__main__":
 # full code: https://github.com/Py-Contributors/audiobook
 # Pypi: https://pypi.org/project/audiobook/
 
-# requirements.txt audiobook==2.0.0
+# requirements.txt audiobook==2.0.2
