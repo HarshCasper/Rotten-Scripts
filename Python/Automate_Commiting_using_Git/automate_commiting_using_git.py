f9a7d8c98e5bfd75fa783ad7fb0d90aa77529f57
Kumar Abhinav <76863411+geekabhinav007@users.noreply.github.com>
2023-05-08 00:44:31 +0530

    Enhanced Git automation script with user input options
    Improved git commit and push automation with customizable commit message, remote name, and branch name with default value for each option.

--- Python/Automate_Commiting_using_Git/automate_commiting_using_git.py
+++ Python/Automate_Commiting_using_Git/automate_commiting_using_git.py
@@ -1,7 +1,37 @@
 import subprocess
 
-subprocess.getoutput("git add .")
-message = input("Enter commit message")
-subprocess.getoutput("git commit -m " + message)
-branchname = input("Enter branch name")
-subprocess.getoutput("git push origin " + branchname)
+# Stage all changes
+result = subprocess.run(["git", "add", "."])
+if result.returncode != 0:
+    print("Error: Failed to stage changes.")
+    exit(1)
+
+# Get commit message from user input or use default value
+message = input("Enter commit message (or press Enter to use default): ")
+if not message:
+    message = "Auto commit"
+
+# Get remote name from user input or use default value
+remote = input("Enter remote name (or press Enter to use default 'origin'): ")
+if not remote:
+    remote = "origin"
+
+# Get branch name from user input or use default value
+branchname = input("Enter branch name (or press Enter to use default 'main'): ")
+if not branchname:
+    branchname = "main"
+
+# Commit changes
+result = subprocess.run(["git", "commit", "-m", message])
+if result.returncode != 0:
+    print("Error: Failed to commit changes.")
+    exit(1)
+
+# Push changes to remote
+result = subprocess.run(["git", "push", remote, branchname])
+if result.returncode != 0:
+    print("Error: Failed to push changes.")
+    exit(1)
+
+print("Changes successfully committed and pushed to remote.")
+
