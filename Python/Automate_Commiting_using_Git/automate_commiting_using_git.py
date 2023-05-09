6fc329b3e8c6c262204e57c4052ffe49a6e21d87
Kumar Abhinav <76863411+geekabhinav007@users.noreply.github.com>
2023-05-10 02:59:16 +0530

    changes exit() to sys.exit() and default branch to current branch

--- Python/Automate_Commiting_using_Git/automate_commiting_using_git.py
+++ Python/Automate_Commiting_using_Git/automate_commiting_using_git.py
@@ -4,7 +4,7 @@ import subprocess
 result = subprocess.run(["git", "add", "."])
 if result.returncode != 0:
     print("Error: Failed to stage changes.")
-    exit(1)
+    sys.exit(1)
 
 # Get commit message from user input or use default value
 message = input("Enter commit message (or press Enter to use default): ")
@@ -15,23 +15,27 @@ if not message:
 remote = input("Enter remote name (or press Enter to use default 'origin'): ")
 if not remote:
     remote = "origin"
-
-# Get branch name from user input or use default value
-branchname = input("Enter branch name (or press Enter to use default 'main'): ")
+    
+# Get current branch name
+result = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, text=True)
+if result.returncode != 0:
+    print("Error: Failed to get current branch name.")
+    exit(1)
+branchname = input("Enter branch name (or press Enter to use default '{}'): ".format(result.stdout.strip()))
 if not branchname:
-    branchname = "main"
+    branchname = result.stdout.strip()
 
 # Commit changes
 result = subprocess.run(["git", "commit", "-m", message])
 if result.returncode != 0:
     print("Error: Failed to commit changes.")
-    exit(1)
+    sys.exit(1)
 
 # Push changes to remote
 result = subprocess.run(["git", "push", remote, branchname])
 if result.returncode != 0:
     print("Error: Failed to push changes.")
-    exit(1)
+    sys.exit(1)
 
 print("Changes successfully committed and pushed to remote.")
 
